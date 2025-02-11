21b90d9d6c9f2be8a801150142a9dd311f7f5f32
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-11 18:43:53 +0100

    Redirect unauthorized users to sign-in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 export default async function Home() {
+	const supabase = await createClient()
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+	if (!user) {
+		redirect('/sign-in')
+	}
 	return (
 		<>
 			<main className='flex-1 flex flex-col gap-6 h-screen px-4'>main page</main>
