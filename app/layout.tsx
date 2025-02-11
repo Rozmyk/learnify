6e18016ac2111ec33f474717d8e1091c98e35973
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-11 18:38:17 +0100

    Change default theme and delete gap

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,19 +29,21 @@ export default function RootLayout({
 	return (
 		<html lang='en' className={geistSans.className} suppressHydrationWarning>
 			<body className='bg-background text-foreground'>
-				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+				<ThemeProvider attribute='class' defaultTheme='light' enableSystem disableTransitionOnChange>
 					<main className='min-h-screen flex flex-col items-center'>
-						<div className='flex-1 w-full flex flex-col gap-20 items-center'>
+						<div className='flex-1 w-full flex flex-col  items-center'>
 							<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
 								<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
 									<div className='flex gap-5 items-center font-semibold'>
-										<Link href={'/'}>Learnify</Link>
+										<Link className='text-xl' href={'/'}>
+											Learnify
+										</Link>
 										<div className='flex items-center gap-2'></div>
 									</div>
 									{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
 								</div>
 							</nav>
-							<div className='flex flex-col gap-20 max-w-5xl p-5'>{children}</div>
+							<div className='flex flex-col gap-20 max-w-7xl w-full '>{children}</div>
 						</div>
 					</main>
 				</ThemeProvider>
