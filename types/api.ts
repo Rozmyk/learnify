5f6fd2732540149265ffa1df26f45eaf2eb51cfc
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-12 20:40:59 +0100

    Add review props

--- types/api.ts
+++ types/api.ts
@@ -6,4 +6,13 @@ export interface CourseProps {
 	created_at: string
 	author_id: string
 	id: string
+	avgRating: number | null
+	reviewCount: number | null
+}
+export interface ReviewProps {
+	rating: number
+	author_id: string
+	course_id: string
+	content: string
+	created_at: string
 }
