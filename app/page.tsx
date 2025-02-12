7c9e6d2c023130aaebf77f0b4d6cc670388d6340
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-12 10:26:53 +0100

    Add protecetedHeader and download first course

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
+
+import ProtectedHeader from '@/components/ProtectedHeader/ProtecetedHeader'
 export default async function Home() {
 	const supabase = await createClient()
+	const { data: courses, error } = await supabase.from('course').select()
 
 	const {
 		data: { user },
@@ -11,7 +14,10 @@ export default async function Home() {
 	}
 	return (
 		<>
-			<main className='flex-1 flex flex-col gap-6 h-screen px-4'>main page</main>
+			<main className='flex-1 flex flex-col gap-6 h-screen p-4'>
+				<ProtectedHeader />
+				{courses && courses.map(course => <div key={course.id}>{course.title}</div>)}
+			</main>
 		</>
 	)
 }
