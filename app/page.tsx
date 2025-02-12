e2e8448ddbad5ffca60b4062d7594480455d78c7
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-12 20:41:58 +0100

    Fetch courses with reviews and calculate average rating and review count

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,22 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import RecommendedCourses from '@/components/ProtectedHeader/RecommendedCourses/RecommendedCourses'
 import ProtectedHeader from '@/components/ProtectedHeader/ProtecetedHeader'
+import { ReviewProps } from '@/types/api'
 export default async function Home() {
 	const supabase = await createClient()
-	const { data: courses, error } = await supabase.from('course').select()
+	const { data: courses, error } = await supabase.from('course').select(`
+    *,
+    reviews(*)
+  `)
+
+	if (error) console.error(error)
+
+	const coursesWithAvgRating = courses?.map(course => {
+		const reviews = course.reviews || []
+		const avgRating =
+			reviews.length > 0 ? reviews.reduce((sum: number, r: ReviewProps) => sum + r.rating, 0) / reviews.length : null
+		return { ...course, avgRating, reviewCount: reviews.length }
+	})
 
 	const {
 		data: { user },
@@ -16,7 +29,7 @@ export default async function Home() {
 		<>
 			<main className='flex-1 flex flex-col gap-6 h-screen p-4'>
 				<ProtectedHeader />
-				{courses && <RecommendedCourses courses={courses} />}
+				{coursesWithAvgRating && <RecommendedCourses courses={coursesWithAvgRating} />}
 			</main>
 		</>
 	)
