706c63ea5aa4f50b9ffa7ab90ff5049eb18f7418
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 18:10:34 +0100

    Fetch userData from profile and add user image

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,10 +1,9 @@
 import { signOutAction } from '@/app/actions'
-import { hasEnvVars } from '@/utils/supabase/check-env-vars'
 import Link from 'next/link'
-import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { createClient } from '@/utils/supabase/server'
 import { ThemeSwitcher } from './theme-switcher'
+import Image from 'next/image'
 
 export default async function AuthButton() {
 	const supabase = await createClient()
@@ -13,21 +12,33 @@ export default async function AuthButton() {
 		data: { user },
 	} = await supabase.auth.getUser()
 
-	return user ? (
+	if (!user) {
+		return (
+			<div className='flex gap-2'>
+				<Button asChild size='sm' variant={'default'}>
+					<Link href='/sign-up'>Sign up</Link>
+				</Button>
+				<ThemeSwitcher />
+			</div>
+		)
+	}
+
+	const { data: profile, error } = await supabase.from('profiles').select('*').eq('id', user.id).single()
+
+	if (error) {
+		console.error('Error fetching profile:', error)
+	}
+
+	return (
 		<div className='flex items-center gap-4'>
 			<ThemeSwitcher />
+			<Image height={30} width={30} src={profile?.avatar_url} alt='User avatar' className='rounded-full' />
+
 			<form action={signOutAction}>
 				<Button type='submit' variant={'outline'}>
 					Sign out
 				</Button>
 			</form>
-		</div>
-	) : (
-		<div className='flex gap-2'>
-			<Button asChild size='sm' variant={'default'}>
-				<Link href='/sign-up'>Sign up</Link>
-			</Button>
-			<ThemeSwitcher />
 		</div>
 	)
 }
