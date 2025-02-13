381fffbd8f5a6c9d4ac2ce80d0fa89613e2522d8
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 15:09:01 +0100

    Fix type error

--- components/ProtectedHeader/ProtecetedHeader.tsx
+++ components/ProtectedHeader/ProtecetedHeader.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import WelcomeMessage from './WelcomeMessage/WelcomeMessage'
 import { ProfileDataProps } from '@/types/api'
-const ProtectedHeader = ({ profileData }: ProfileDataProps) => {
+const ProtectedHeader = ({ profileData }: { profileData: ProfileDataProps }) => {
 	return (
 		<div>
 			<WelcomeMessage username={profileData.username} userPhoto={profileData.avatar_url} />
 			<div className='flex justify-between items-center w-full'>
 				<p>Start learning</p>
-				<Link href={'/courses'}>Your courses</Link>
+				<Link href={'/courses'}>Your courses:</Link>
 			</div>
 		</div>
 	)
