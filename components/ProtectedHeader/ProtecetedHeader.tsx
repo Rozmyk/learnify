adc05cab12a3df3e6e3d975e8e0683805755ffbb
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 15:06:58 +0100

    Add transfer props to WelcomeMessage

--- components/ProtectedHeader/ProtecetedHeader.tsx
+++ components/ProtectedHeader/ProtecetedHeader.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import WelcomeMessage from './WelcomeMessage/WelcomeMessage'
-const ProtectedHeader = () => {
+import { ProfileDataProps } from '@/types/api'
+const ProtectedHeader = ({ profileData }: ProfileDataProps) => {
 	return (
 		<div>
-			<WelcomeMessage username='Rozmyk' userPhoto='' />
+			<WelcomeMessage username={profileData.username} userPhoto={profileData.avatar_url} />
 			<div className='flex justify-between items-center w-full'>
 				<p>Start learning</p>
 				<Link href={'/courses'}>Your courses</Link>
