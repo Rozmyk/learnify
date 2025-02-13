28666b69b1a6d738fc784f6b172796dab7dc8a52
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 15:06:34 +0100

    Change domains url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ['mvykgvraqeirkazxhitb.supabase.co'],
+		domains: ['wltlbfcgbnhlxamxxnve.supabase.co'],
 	},
 }
 
