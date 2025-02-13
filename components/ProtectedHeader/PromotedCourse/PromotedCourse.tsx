0d99c5efed1daf67ea6a0ac26b6a67710049d5bd
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 14:25:11 +0100

    add StarRating to card

--- components/ProtectedHeader/PromotedCourse/PromotedCourse.tsx
+++ components/ProtectedHeader/PromotedCourse/PromotedCourse.tsx
@@ -1,5 +1,6 @@
 import { CourseProps } from '@/types/api'
 import Image from 'next/image'
+import StarRating from '@/components/ui/starRating'
 const PromotedCourse = ({ course }: { course: CourseProps }) => {
 	console.log(course)
 	return (
@@ -9,12 +10,16 @@ const PromotedCourse = ({ course }: { course: CourseProps }) => {
 				<div className='w-1/3 h-80  relative rounded-xl overflow-hidden '>
 					<Image fill src={course.thumbnail} alt={`${course.title} course`} />
 				</div>
-				<div className='w-2/3 flex flex-col justify-start items-start h-full '>
-					<h3 className='text-3xl font-bold mb-2 group-hover:text-primary transition-colors duration-300'>
-						{course.title}
-					</h3>
-					<p className='text-muted-foreground mb-4 line-clamp-2 flex-1'>{course.description}</p>
-					<p>Author:</p>
+				<div className='w-2/3 flex flex-col justify-between items-between h-80  	 '>
+					<div>
+						<h3 className='text-3xl font-bold mb-2 group-hover:text-primary transition-colors duration-300'>
+							{course.title}
+						</h3>
+						<p className='text-muted-foreground mb-4 line-clamp-2 flex-1'>{course.description}</p>
+						<p>Author:</p>
+						<StarRating />
+					</div>
+
 					<p className='text-lg font-semibold'>{course.price} zł</p>
 				</div>
 			</div>
