5b2969b5d904ec81256ea4483f67c9fbff340207
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 15:22:18 +0100

    Update div properties and resize image

--- components/ProtectedHeader/WelcomeMessage/WelcomeMessage.tsx
+++ components/ProtectedHeader/WelcomeMessage/WelcomeMessage.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const WelcomeMessage = ({ username, userPhoto }: { username: string; userPhoto: string }) => {
 	return (
-		<div>
-			<Image width={100} height={100} className='rounded-full' src={userPhoto} alt={`${username} user photo`} />
+		<div className='flex justify-start items-center gap-8 mb-8'>
+			<Image width={75} height={75} className='rounded-full' src={userPhoto} alt={`${username} user photo`} />
 			<h1 className='text-3xl font-semibold'>Welcome back {username}!</h1>
 		</div>
 	)
