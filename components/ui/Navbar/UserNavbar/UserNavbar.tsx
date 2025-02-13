76c634bae6ab6911c42016fc6a62f5242c0ab058
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 20:11:29 +0100

    Add ActionMenu

--- components/ui/Navbar/UserNavbar/UserNavbar.tsx
+++ components/ui/Navbar/UserNavbar/UserNavbar.tsx
@@ -2,7 +2,7 @@ import { Button } from '../../button'
 import { ThemeSwitcher } from '@/components/theme-switcher'
 import Image from 'next/image'
 import { createClient } from '@/utils/supabase/server'
-
+import ActionMenu from '../ActionMenu/ActionMenu'
 export default async function UserNavbar({ userId }: { userId: string }) {
 	const supabase = await createClient()
 	const { data: profile, error } = await supabase.from('profiles').select('*').eq('id', userId).single()
@@ -14,9 +14,7 @@ export default async function UserNavbar({ userId }: { userId: string }) {
 		<div className='flex items-center gap-4'>
 			<Button variant={'ghost'}>My training</Button>
 			<ThemeSwitcher />
-			<Button variant={'ghost'}>
-				<Image height={30} width={30} src={profile?.avatar_url} alt='User avatar' className='rounded-full' />
-			</Button>
+			<ActionMenu avatarUrl={profile.avatar_url} />
 		</div>
 	)
 }
