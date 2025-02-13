8973e5c4af200aca6179f61ba508b6eee0f80a56
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 16:16:06 +0100

    Imporve user greeting layout with responsive image and text

--- components/ProtectedHeader/WelcomeMessage/WelcomeMessage.tsx
+++ components/ProtectedHeader/WelcomeMessage/WelcomeMessage.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image'
 
 const WelcomeMessage = ({ username, userPhoto }: { username: string; userPhoto: string }) => {
 	return (
-		<div className='flex justify-start items-center gap-8 mb-8'>
-			<Image width={75} height={75} className='rounded-full' src={userPhoto} alt={`${username} user photo`} />
-			<h1 className='text-3xl font-semibold'>Welcome back {username}!</h1>
+		<div className='flex justify-start items-center  gap-4 md:gap-8 mb-8'>
+			<div className='w-12 h-12 md:h-20 md:w-20 relative'>
+				<Image fill className='rounded-full' src={userPhoto} alt={`${username} user photo`} />
+			</div>
+
+			<h1 className=' text-xl md:text-3xl font-semibold'>Welcome back {username}!</h1>
 		</div>
 	)
 }
