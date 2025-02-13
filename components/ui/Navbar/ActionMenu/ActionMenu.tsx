209c7d2c53190cfc2d9fe0160040091d8f74d7e7
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 20:13:02 +0100

    Create Dropdown menu

--- components/ui/Navbar/ActionMenu/ActionMenu.tsx
+++ components/ui/Navbar/ActionMenu/ActionMenu.tsx
@@ -1,12 +1,20 @@
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuRadioGroup,
-	DropdownMenuRadioItem,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-const ActionMenu = () => {
-	return <div>dropdownmenyu</div>
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Button } from '../../button'
+import Image from 'next/image'
+
+const ActionMenu = ({ avatarUrl }: { avatarUrl: string }) => {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant={'ghost'}>
+					<Image height={30} width={30} src={avatarUrl} alt='User avatar' className='rounded-full' />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className='w-content' align='start'>
+				<DropdownMenuItem>Edit</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
 }
 
 export default ActionMenu
