bd7d34e462a0a6f89f0db9be365a3faf1b7370e3
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-13 17:23:15 +0100

    Fetch categories from Supabase and render as Links

--- components/ui/CategoryMenu/CategoryMenu.tsx
+++ components/ui/CategoryMenu/CategoryMenu.tsx
@@ -1,5 +1,38 @@
+'use client'
+import { createClient } from '@/utils/supabase/client'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { CategoryProps } from '@/types/api'
+
 const CategoryMenu = () => {
-	return <div>CategoryMenu</div>
+	const [categoriesData, setCategoriesData] = useState<null | CategoryProps[]>(null)
+
+	useEffect(() => {
+		const getCategory = async () => {
+			const supabase = createClient()
+			const { data: categories, error } = await supabase.from('categories').select('*')
+			if (error) {
+				console.error('Error during category download:', error.message)
+			} else {
+				setCategoriesData(categories)
+			}
+		}
+
+		getCategory()
+	}, [])
+	return (
+		<div className='h-12  w-full flex justify-center  border-b border-b-foreground/10'>
+			<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
+				{categoriesData?.map(category => {
+					return (
+						<Link href={`courses/${category.slug}`} key={category.id}>
+							{category.name}
+						</Link>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default CategoryMenu
