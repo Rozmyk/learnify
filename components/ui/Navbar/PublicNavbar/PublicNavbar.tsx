0d8142033d775a4c00519088d23fb45f223280dd
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 19:27:56 +0100

    Add home button

--- components/ui/Navbar/PublicNavbar/PublicNavbar.tsx
+++ components/ui/Navbar/PublicNavbar/PublicNavbar.tsx
@@ -1,13 +1,17 @@
 import { Button } from '../../button'
 import Link from 'next/link'
 import { ThemeSwitcher } from '@/components/theme-switcher'
+import HomeButton from '../HomeButton/HomeButton'
 const PublicNavbar = () => {
 	return (
-		<div className='flex gap-2'>
-			<Button asChild size='sm' variant={'default'}>
-				<Link href='/sign-up'>Sign up</Link>
-			</Button>
-			<ThemeSwitcher />
+		<div className='flex justify-between items-center w-full'>
+			<HomeButton />
+			<div className='flex gap-2'>
+				<Button asChild size='sm' variant={'default'}>
+					<Link href='/sign-up'>Sign up</Link>
+				</Button>
+				<ThemeSwitcher />
+			</div>
 		</div>
 	)
 }
