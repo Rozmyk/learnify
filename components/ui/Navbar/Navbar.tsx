4d2f1cd5a89c0ab7718a9e491d347086dced39be
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 15:16:58 +0100

    Split Navbar to desktop and mobile

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 import { createClient } from '@/utils/supabase/server'
 import UserNavbar from './UserNavbar/UserNavbar'
 import PublicNavbar from './PublicNavbar/PublicNavbar'
+import NavbarMobile from './NavbarMobile/NavbarMobile'
 
 export default async function Navbar() {
 	const supabase = await createClient()
 	const {
 		data: { user },
 	} = await supabase.auth.getUser()
+
 	return (
 		<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
 			<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
@@ -15,9 +17,13 @@ export default async function Navbar() {
 					<Link className='text-xl' href={'/'}>
 						Learnify
 					</Link>
-					<div className='flex items-center gap-2'></div>
 				</div>
-				{user ? <UserNavbar userId={user.id} /> : <PublicNavbar />}
+
+				<div className='hidden lg:flex gap-5'>{user ? <UserNavbar userId={user.id} /> : <PublicNavbar />}</div>
+
+				<div className='lg:hidden'>
+					<NavbarMobile />
+				</div>
 			</div>
 		</nav>
 	)
