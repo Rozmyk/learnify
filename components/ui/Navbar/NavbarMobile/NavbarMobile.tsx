50c6c75ce3eff19eefff70a018d038f1a8d6c86c
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 15:46:07 +0100

    Add Icons and change layout

--- components/ui/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/ui/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,14 +1,15 @@
 'use client'
 import Drawer from './Drawer/Drawer'
 import { useState } from 'react'
-
 import Link from 'next/link'
 import HamburgerBtn from './HamburgerBtn/HamburgerBtn'
 import Overlay from './Overlay/Overlay'
 import CloseButton from './CloseButton/CloseButton'
+import { Search, ShoppingBasket } from 'lucide-react'
+import { Button } from '../../button'
 const NavbarMobile = () => {
 	const [isOpen, setIsOpen] = useState(false)
-
+	const ICON_SIZE = 14
 	const handleOpen = () => {
 		setIsOpen(true)
 	}
@@ -18,12 +19,23 @@ const NavbarMobile = () => {
 
 	return (
 		<>
-			<div className='flex gap-5 items-center font-semibold'>
-				<Link className='text-xl' href={'/'}>
-					Learnify
-				</Link>
+			<div className='flex justify-between items-center w-ful '>
+				<HamburgerBtn isOpen={isOpen} handleOpen={handleOpen} />
+				<div className='flex gap-5 items-center font-semibold '>
+					<Link className='text-xl' href={'/'}>
+						Learnify
+					</Link>
+				</div>
+				<div className='flex   '>
+					<Button size='icon' variant='ghost'>
+						<Search size={ICON_SIZE} />
+					</Button>
+					<Button size='icon' variant='ghost'>
+						<ShoppingBasket size={ICON_SIZE} />
+					</Button>
+				</div>
 			</div>
-			<HamburgerBtn isOpen={isOpen} handleOpen={handleOpen} />
+
 			<Overlay isOpen={isOpen} handleClose={handleClose} />
 			<Drawer isOpen={isOpen} />
 			<CloseButton isOpen={isOpen} handleClose={handleClose} />
