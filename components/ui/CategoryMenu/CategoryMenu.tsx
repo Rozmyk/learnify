ae15bf2fe8f403aee0fdd5562ced44f514fb6b8a
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 17:35:05 +0100

    Fetch categories data from endpoint

--- components/ui/CategoryMenu/CategoryMenu.tsx
+++ components/ui/CategoryMenu/CategoryMenu.tsx
@@ -1,43 +1,48 @@
 'use client'
-import { createClient } from '@/utils/supabase/client'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { CategoryProps } from '@/types/api'
 import { usePathname } from 'next/navigation'
 import { Button } from '../button'
+
 const CategoryMenu = () => {
 	const [categoriesData, setCategoriesData] = useState<null | CategoryProps[]>(null)
 	const pathname = usePathname()
 
 	useEffect(() => {
-		const getCategory = async () => {
-			const supabase = createClient()
-			const { data: categories, error } = await supabase.from('categories').select('*')
-			if (error) {
-				console.error('Error during category download:', error.message)
-			} else {
-				setCategoriesData(categories)
+		const getCategories = async () => {
+			try {
+				const response = await fetch('/api/categories')
+
+				if (!response.ok) {
+					throw new Error('Failed to fetch categories')
+				}
+
+				const data = await response.json()
+				setCategoriesData(data)
+			} catch (error) {
+				console.error('Error during category download:', error)
 			}
 		}
 
-		getCategory()
+		getCategories()
 	}, [])
+
 	if (pathname !== '/') {
 		return null
 	}
+
 	return (
 		<div className='w-full hidden lg:block'>
-			<div className='h-12  w-full flex justify-center  border-b border-b-foreground/10 '>
+			<div className='h-12 w-full flex justify-center border-b border-b-foreground/10'>
 				<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-					{categoriesData?.map(category => {
-						return (
-							<Button variant='ghost' key={category.id}>
-								<Link className='hover:text-white' href={`courses/${category.slug}`}>
-									{category.name}
-								</Link>
-							</Button>
-						)
-					})}
+					{categoriesData?.map(category => (
+						<Button variant='ghost' key={category.id}>
+							<Link className='hover:text-white' href={`courses/${category.slug}`}>
+								{category.name}
+							</Link>
+						</Button>
+					))}
 				</div>
 			</div>
 		</div>
