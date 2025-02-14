f62fb57c6f2cc8d1ca44f21bd6e4d8a79302c722
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 17:11:06 +0100

    Add link component to HomeButton

--- components/ui/Navbar/HomeButton/HomeButton.tsx
+++ components/ui/Navbar/HomeButton/HomeButton.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link'
 const HomeButton = () => {
-    return (  );
+	return (
+		<div className='flex gap-5 items-center font-semibold '>
+			<Link className='text-xl' href={'/'}>
+				Learnify
+			</Link>
+		</div>
+	)
 }
- 
-export default HomeButton;
+
+export default HomeButton
