640148f14b780153668de8470231c859e5df2845
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 20:22:03 +0100

    Update styles

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -4,44 +4,45 @@ import { SubmitButton } from '@/components/submit-button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
-import { SmtpMessage } from '../smtp-message'
 
 export default async function Signup(props: { searchParams: Promise<Message> }) {
 	const searchParams = await props.searchParams
+
 	if ('message' in searchParams) {
 		return (
-			<div className='w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4'>
+			<div className='w-full flex-1 flex items-center min-h-[calc(100vh-4rem)] justify-center gap-2 p-4'>
 				<FormMessage message={searchParams} />
 			</div>
 		)
 	}
 
 	return (
-		<>
-			<form className='flex flex-col min-w-64 max-w-64 mx-auto'>
-				<h1 className='text-2xl font-medium'>Sign up</h1>
-				<p className='text-sm text text-foreground'>
-					Already have an account?{' '}
-					<Link className='text-primary font-medium underline' href='/sign-in'>
-						Sign in
-					</Link>
-				</p>
-				<div className='flex flex-col gap-2 [&>input]:mb-3 mt-8'>
-					<Label htmlFor='email'>Email</Label>
-					<Input name='email' placeholder='you@example.com' required />
+		<div className='min-h-[calc(100vh-4rem)] flex items-center justify-center'>
+			<div className='border border-border w-[30rem] p-4 rounded-xl flex flex-col gap-4'>
+				<form className='flex flex-col w-full '>
+					<h1 className='text-2xl font-medium'>Sign up</h1>
+					<p className='text-sm text-foreground'>
+						Already have an account?{' '}
+						<Link className='text-primary font-medium underline' href='/sign-in'>
+							Sign in
+						</Link>
+					</p>
+					<div className='flex flex-col gap-2 [&>input]:mb-3 mt-8'>
+						<Label htmlFor='email'>Email</Label>
+						<Input name='email' placeholder='you@example.com' required />
 
-					<Label htmlFor='username'>Username</Label>
-					<Input name='username' placeholder='Riqal' required />
+						<Label htmlFor='username'>Username</Label>
+						<Input name='username' placeholder='Riqal' required />
 
-					<Label htmlFor='password'>Password</Label>
-					<Input type='password' name='password' placeholder='Your password' minLength={6} required />
-					<SubmitButton formAction={signUpAction} pendingText='Signing up...'>
-						Sign up
-					</SubmitButton>
-					<FormMessage message={searchParams} />
-				</div>
-			</form>
-			<SmtpMessage />
-		</>
+						<Label htmlFor='password'>Password</Label>
+						<Input type='password' name='password' placeholder='Your password' minLength={6} required />
+						<SubmitButton formAction={signUpAction} pendingText='Signing up...'>
+							Sign up
+						</SubmitButton>
+						<FormMessage message={searchParams} />
+					</div>
+				</form>
+			</div>
+		</div>
 	)
 }
