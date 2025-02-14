65cf04dd29707422852a9042ad6185a46b9634a7
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 15:16:40 +0100

    Add hamburger btn and Drawer with logic

--- components/ui/Navbar/NavbarMobile/NavbarMobile.tsx
+++ components/ui/Navbar/NavbarMobile/NavbarMobile.tsx
@@ -1,5 +1,43 @@
+'use client'
+import Drawer from './Drawer/Drawer'
+import { useState } from 'react'
+import { Button } from '../../button'
 const NavbarMobile = () => {
-	return <div>navbarMobile</div>
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleOpen = () => {
+		setIsOpen(true)
+	}
+	const handleClose = () => {
+		setIsOpen(false)
+	}
+
+	return (
+		<>
+			<button onClick={handleOpen} className='flex flex-col justify-center items-center p-2'>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+						isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
+					}`}></span>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+						isOpen ? 'opacity-0' : 'opacity-100'
+					}`}></span>
+				<span
+					className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+						isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
+					}`}></span>
+			</button>
+
+			{isOpen && <div className='fixed inset-0 bg-black bg-opacity-50 z-30' onClick={handleClose} />}
+			<Drawer isOpen={isOpen} />
+			{isOpen && (
+				<Button onClick={handleClose} className='fixed left-[18rem] top-4 z-50 '>
+					X
+				</Button>
+			)}
+		</>
+	)
 }
 
 export default NavbarMobile
