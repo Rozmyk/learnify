42f573edabf87adf9c7b98d20a10eb04be1bbddd
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 17:07:31 +0100

    Add Welcome message to auth user

--- components/ui/Navbar/NavbarMobile/Drawer/Drawer.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/Drawer.tsx
@@ -1,11 +1,26 @@
-const Drawer = ({ isOpen }: { isOpen: boolean }) => {
+import { ProfileDataProps } from '@/types/api'
+import { ChevronRight } from 'lucide-react'
+import Image from 'next/image'
+
+const Drawer = ({ isOpen, user }: { isOpen: boolean; user: ProfileDataProps | null }) => {
 	return (
 		<div
 			className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-card w-64 dark:bg-card ${
 				isOpen ? 'translate-x-0' : '-translate-x-full'
 			}`}
 			aria-labelledby='drawer-navigation-label'>
-			<p className='text-black dark:text-white'>drawer content</p>
+			{user ? (
+				<div className='flex justify-center items-center gap-4'>
+					<Image className='rounded-full' src={user.avatar_url} width={60} height={60} alt='User avatar' />
+					<div>
+						<p className='font-semibold'>Hello, {user.username}</p>
+						<p>Welcome back!</p>
+					</div>
+					<ChevronRight size={18} />
+				</div>
+			) : (
+				<p>register</p>
+			)}
 		</div>
 	)
 }
