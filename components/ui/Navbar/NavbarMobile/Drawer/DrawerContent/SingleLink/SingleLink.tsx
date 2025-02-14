386ac34fe97fb7449844800a1ff386ff0973e20b
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 20:23:58 +0100

    Update styles

--- components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/SingleLink/SingleLink.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/SingleLink/SingleLink.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const SingleLink = ({ href, content }: { href: string; content: string }) => {
 	return (
-		<Link className='py-1 px-0.5 ' href={href}>
+		<Link className='py-1 px-0.5 text-md' href={href}>
 			{content}
 		</Link>
 	)
