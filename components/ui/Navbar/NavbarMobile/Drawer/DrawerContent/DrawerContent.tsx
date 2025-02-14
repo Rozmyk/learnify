6a5ea65b7ba460e4fbaa57a56b0e7f66a4ccb576
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 17:50:25 +0100

    Add fetch data from categories endpoint

--- components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/DrawerContent.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/DrawerContent.tsx
@@ -1,5 +1,36 @@
+'use client'
+import { useState, useEffect } from 'react'
+import { CategoryProps } from '@/types/api'
+import MostPopular from './MostPopular/MostPopular'
+import Explore from './Explore/Explore'
+import OtherServices from './OtherServices/OtherServices'
 const DrawerContent = () => {
-	return <div></div>
+	const [categoriesData, setCategoriesData] = useState<null | CategoryProps[]>(null)
+	useEffect(() => {
+		const getCategories = async () => {
+			try {
+				const response = await fetch('/api/categories')
+
+				if (!response.ok) {
+					throw new Error('Failed to fetch categories')
+				}
+
+				const data = await response.json()
+				setCategoriesData(data)
+			} catch (error) {
+				console.error('Error during category download:', error)
+			}
+		}
+
+		getCategories()
+	}, [])
+	return (
+		<div>
+			<Explore />
+			<MostPopular categoriesData={categoriesData} />
+			<OtherServices />
+		</div>
+	)
 }
 
 export default DrawerContent
