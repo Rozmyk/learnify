1311b353c2574e467788950df6bb9f45861d07c3
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 18:42:34 +0100

    Add padding

--- components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/SingleLink/SingleLink.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/DrawerContent/SingleLink/SingleLink.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 const SingleLink = ({ href, content }: { href: string; content: string }) => {
-	return <Link href={href}>{content}</Link>
+	return (
+		<Link className='py-1 px-0.5 ' href={href}>
+			{content}
+		</Link>
+	)
 }
 
 export default SingleLink
