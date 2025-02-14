6630b04a69df47cbb2384da1dd5b404af8bbf3c3
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 15:45:55 +0100

    Add full width to navbar Mobile

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -13,15 +13,9 @@ export default async function Navbar() {
 	return (
 		<nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
 			<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-				<div className='flex gap-5 items-center font-semibold'>
-					<Link className='text-xl' href={'/'}>
-						Learnify
-					</Link>
-				</div>
-
 				<div className='hidden lg:flex gap-5'>{user ? <UserNavbar userId={user.id} /> : <PublicNavbar />}</div>
 
-				<div className='lg:hidden'>
+				<div className='lg:hidden w-full'>
 					<NavbarMobile />
 				</div>
 			</div>
