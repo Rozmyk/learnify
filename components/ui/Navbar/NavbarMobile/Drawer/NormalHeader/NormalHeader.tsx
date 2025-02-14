5eb90da983c3eb353493d67fb73e8b706893cfcb
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 19:01:47 +0100

    Add 2 buttons with redirect to sign-in and sign-up

--- components/ui/Navbar/NavbarMobile/Drawer/NormalHeader/NormalHeader.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/NormalHeader/NormalHeader.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button'
-
+import Link from 'next/link'
 const NormalHeader = () => {
 	return (
-		<div>
-			<Button>Login</Button>
-			<Button>Register</Button>
+		<div className='flex flex-col gap-2 mb-4 '>
+			<Button variant='outline'>
+				<Link href={'/sign-in'}>Login</Link>
+			</Button>
+			<Button variant='outline'>
+				<Link href='/sign-up'>Register</Link>
+			</Button>
 		</div>
 	)
 }
