39a79fd99bdca42b099a612aee95972f78859a6e
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 18:52:42 +0100

    Create NormalHeader.tsx

---
+++ components/ui/Navbar/NavbarMobile/Drawer/NormalHeader/NormalHeader.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+const NormalHeader = () => {
+	return (
+		<div>
+			<Button>Login</Button>
+			<Button>Register</Button>
+		</div>
+	)
+}
+
+export default NormalHeader
