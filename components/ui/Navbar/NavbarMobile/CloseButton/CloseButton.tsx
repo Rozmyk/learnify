a0a3968fee2ea4d0d8cd8a59ef3b3e04b7b7b04d
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-14 17:07:19 +0100

    Add Icon

--- components/ui/Navbar/NavbarMobile/CloseButton/CloseButton.tsx
+++ components/ui/Navbar/NavbarMobile/CloseButton/CloseButton.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@/components/ui/button'
+import { X } from 'lucide-react'
 const CloseButton = ({ isOpen, handleClose }: { isOpen: boolean; handleClose: () => void }) => {
 	return (
 		isOpen && (
-			<Button variant='outline' onClick={handleClose} className='fixed left-[18rem] top-4 z-50 rounded-full '>
-				X
+			<Button
+				variant='outline'
+				size='icon'
+				onClick={handleClose}
+				className='fixed left-[18rem] top-4 z-50 rounded-full '>
+				<X size={16} />
 			</Button>
 		)
 	)
