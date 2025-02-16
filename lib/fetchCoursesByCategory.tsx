eda7aec2d70ceab3828ace9cec9997d4c8c7374f
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-16 17:56:18 +0100

    Create fetchCoursesByCategory.tsx

---
+++ lib/fetchCoursesByCategory.tsx
@@ -0,0 +1,24 @@
+import { createClient } from '@/utils/supabase/server'
+import { addRatingsToCourses } from './calcRatings'
+export async function fetchCoursesByCategory(categoryId: string) {
+	const supabase = await createClient()
+
+	const { data: courses, error } = await supabase
+		.from('course')
+		.select(
+			`
+      *,
+      reviews(*),
+      categories!inner(*),
+      profiles(*)
+      `
+		)
+		.eq('categories.id', categoryId)
+
+	if (error) {
+		console.error('Error fetching courses by category:', error)
+		return []
+	}
+	const updatedCourses = addRatingsToCourses(courses)
+	return updatedCourses
+}
