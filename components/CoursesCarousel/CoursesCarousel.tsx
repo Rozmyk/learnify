2cd4f670c40c7c217e62dd91b1025cd61ce75e72
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 16:46:42 +0100

    Create CoursesCarousel.tsx

---
+++ components/CoursesCarousel/CoursesCarousel.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import CourseCard from '../ui/CourseCard'
+import useEmblaCarousel from 'embla-carousel-react'
+import { CourseProps } from '@/types/api'
+import { PrevButton, NextButton, usePrevNextButtons } from './Buttons/Buttons'
+
+type PropType = {
+	courses: CourseProps[]
+	text: string
+	options?: EmblaOptionsType
+}
+
+const CoursesCarousel: React.FC<PropType> = props => {
+	const { courses, options, text } = props
+	const [emblaRef, emblaApi] = useEmblaCarousel(options)
+	const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi)
+
+	return (
+		<section className='embla'>
+			<div className='flex justify-between items-center '>
+				<p className=' text-2xl font-semibold '>{text}</p>
+				<div className='embla__controls  '>
+					<div className='embla__buttons '>
+						<PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+						<NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+					</div>
+				</div>
+			</div>
+			<div className='embla__viewport' ref={emblaRef}>
+				<div className='embla__container'>
+					{courses &&
+						courses.map(course => (
+							<div className='embla__slide ' key={course.id}>
+								<CourseCard {...course} />
+							</div>
+						))}
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default CoursesCarousel
