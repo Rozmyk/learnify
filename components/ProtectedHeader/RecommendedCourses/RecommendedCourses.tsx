c4ef645c5ddc38d12f4d4bf5575e3ad5e21a7230
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 16:47:00 +0100

    Use CoursesCarousel

--- components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
+++ components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
@@ -1,13 +1,12 @@
-import CaourseCard from '@/components/ui/CourseCard'
 import { CourseProps } from '@/types/api'
+import CoursesCarousel from '@/components/CoursesCarousel/CoursesCarousel'
 const RecommendedCourses = ({ courses }: { courses: CourseProps[] }) => {
 	return (
 		<div>
 			<h2 className='text-4xl mb-5'>What will you learn now?</h2>
 			<p className='mb-10 text-2xl'>Recommended for you</p>
-			<div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
-				{courses && courses.map(course => <CaourseCard key={course.id} {...course} />)}
-			</div>
+
+			<CoursesCarousel text='Recommended for you' courses={courses} />
 		</div>
 	)
 }
