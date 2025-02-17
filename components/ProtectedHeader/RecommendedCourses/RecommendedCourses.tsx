2c8ab6c3322ef4e1edf0760e365b819936a70439
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 16:58:27 +0100

    Delete old text

--- components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
+++ components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
@@ -4,7 +4,6 @@ const RecommendedCourses = ({ courses }: { courses: CourseProps[] }) => {
 	return (
 		<div>
 			<h2 className='text-4xl mb-5'>What will you learn now?</h2>
-			<p className='mb-10 text-2xl'>Recommended for you</p>
 
 			<CoursesCarousel text='Recommended for you' courses={courses} />
 		</div>
