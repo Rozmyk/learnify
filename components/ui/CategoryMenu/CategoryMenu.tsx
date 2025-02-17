e8548014ff98f1a486596671b6e889518e305365
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 13:48:49 +0100

    Add loading

--- components/ui/CategoryMenu/CategoryMenu.tsx
+++ components/ui/CategoryMenu/CategoryMenu.tsx
@@ -4,9 +4,11 @@ import Link from 'next/link'
 import { CategoryProps } from '@/types/api'
 import { usePathname } from 'next/navigation'
 import { Button } from '../button'
+import Loading from './Loading/Loading'
 
 const CategoryMenu = () => {
 	const [categoriesData, setCategoriesData] = useState<null | CategoryProps[]>(null)
+	const [loading, setLoading] = useState(true)
 	const pathname = usePathname()
 
 	useEffect(() => {
@@ -20,6 +22,7 @@ const CategoryMenu = () => {
 
 				const data = await response.json()
 				setCategoriesData(data)
+				setLoading(false)
 			} catch (error) {
 				console.error('Error during category download:', error)
 			}
@@ -36,13 +39,17 @@ const CategoryMenu = () => {
 		<div className='w-full hidden lg:block'>
 			<div className='h-12 w-full flex justify-center border-b border-b-foreground/10'>
 				<div className='w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm'>
-					{categoriesData?.map(category => (
-						<Button variant='ghost' key={category.id}>
-							<Link className='hover:text-white' href={`courses/${category.slug}`}>
-								{category.name}
-							</Link>
-						</Button>
-					))}
+					{loading ? (
+						<Loading />
+					) : (
+						categoriesData?.map(category => (
+							<Button variant='ghost' key={category.id}>
+								<Link className='hover:text-white' href={`courses/${category.slug}`}>
+									{category.name}
+								</Link>
+							</Button>
+						))
+					)}
 				</div>
 			</div>
 		</div>
