a74c11b4e0be19a3b20936caab9b480816b7f32c
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 13:09:25 +0100

    Add check and conversion for course to array

--- lib/calcRatings.tsx
+++ lib/calcRatings.tsx
@@ -1,23 +1,13 @@
 import { CourseProps, ReviewProps } from '@/types/api'
 
-type CoursesInput = CourseProps | CourseProps[]
+export function addRatingsToCourses(courses: CourseProps | CourseProps[]): CourseProps[] {
+	const coursesArray = Array.isArray(courses) ? courses : [courses]
 
-export function addRatingsToCourses(courses: CoursesInput): CoursesInput {
-	const calculateRatings = (course: CourseProps): CourseProps => {
+	return coursesArray.map(course => {
 		const reviews: ReviewProps[] = course.reviews || []
 		const reviewCount = reviews.length
 		const avgRating = reviewCount > 0 ? reviews.reduce((sum, review) => sum + review.rating, 0) / reviewCount : null
 
 		return { ...course, avgRating, reviewCount }
-	}
-
-	if (Array.isArray(courses)) {
-		return courses.map(calculateRatings)
-	}
-
-	if (courses) {
-		return calculateRatings(courses)
-	}
-
-	return courses
+	})
 }
