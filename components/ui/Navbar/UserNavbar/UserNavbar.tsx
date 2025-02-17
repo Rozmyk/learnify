2ae8bfe38070774ef5c9eddbfb4c55df4809bf39
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-17 18:22:08 +0100

    Add Link to buttons

--- components/ui/Navbar/UserNavbar/UserNavbar.tsx
+++ components/ui/Navbar/UserNavbar/UserNavbar.tsx
@@ -3,13 +3,20 @@ import { ThemeSwitcher } from '@/components/theme-switcher'
 import ActionMenu from '../ActionMenu/ActionMenu'
 import { ProfileDataProps } from '@/types/api'
 import HomeButton from '../HomeButton/HomeButton'
+import Link from 'next/link'
 export default async function UserNavbar({ user }: { user: ProfileDataProps }) {
 	return (
 		<div className='flex justify-between items-center  gap-4 w-full'>
 			<HomeButton />
 			<div className='flex items-center'>
-				{user.isTeacher && <Button>Add course</Button>}
-				<Button variant={'ghost'}>My courses</Button>
+				{user.isTeacher && (
+					<Link href={'/add-course'}>
+						<Button variant='ghost'>Add course</Button>
+					</Link>
+				)}
+				<Link href='/course'>
+					<Button variant={'ghost'}>My courses</Button>
+				</Link>
 				<ThemeSwitcher />
 				<ActionMenu avatarUrl={user.avatar_url} username={user.username} email={user.email} />
 			</div>
