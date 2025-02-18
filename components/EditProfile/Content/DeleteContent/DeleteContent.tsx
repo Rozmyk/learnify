828c20735154be65c33aad388c0781d869cd5561
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-18 18:15:40 +0100

    Create DeleteContent.tsx

---
+++ components/EditProfile/Content/DeleteContent/DeleteContent.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+const DeleteContent = () => {
+	return (
+		<div className='flex flex-col justify-start items-start w-full '>
+			<p className='mb-8'>
+				<span className='text-red-400'>Warning!</span> If you close your account, we will unsubscribe you from all
+				courses you attend and you will forever lose access to your account and associated data, even if you create a
+				new account using the same email address in the future.
+			</p>
+			<Button>Delete account</Button>
+		</div>
+	)
+}
+
+export default DeleteContent
