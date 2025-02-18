4c311ff3196d7624cc3cb225b9039654ad209e2b
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-18 16:49:14 +0100

    add component EditProfile

--- app/edit-profile/page.tsx
+++ app/edit-profile/page.tsx
@@ -1,6 +1,21 @@
-const page = () => {
-	return <div>edit profile</div>
-}
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+import { fetchUserProfileData } from '@/lib/fetchUserProfileData'
 
-export default page
-;<div>edit profile</div>
+import { ProfileDataProps } from '@/types/api'
+import ProfileSidebar from '@/components/EditProfile/ProfileSidebar/ProfileSidebar'
+import EditProfile from '@/components/EditProfile/EditProfile'
+
+export default async function EditProfilePage() {
+	const supabase = await createClient()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+
+	if (!user) {
+		redirect('/')
+	}
+	const profileData: ProfileDataProps = await fetchUserProfileData(user.id)
+
+	return <EditProfile profileData={profileData} />
+}
