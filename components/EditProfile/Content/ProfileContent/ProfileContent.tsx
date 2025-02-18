ae1ce749730cbf5cfccd18e0f5c2713b06bf9e91
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-18 18:16:05 +0100

    Add Input with labels

--- components/EditProfile/Content/ProfileContent/ProfileContent.tsx
+++ components/EditProfile/Content/ProfileContent/ProfileContent.tsx
@@ -1,5 +1,18 @@
-const ProfileContent = () => {
-	return <div>profile</div>
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { ProfileDataProps } from '@/types/api'
+const ProfileContent = ({ profileData }: { profileData: ProfileDataProps }) => {
+	return (
+		<div className='flex flex-col justify-start items-start w-full '>
+			<Label className='mb-4'>Basic information</Label>
+			<Input className='mb-4' placeholder='Username' defaultValue={profileData.username} />
+			<Input className='mb-8' placeholder='Description' />
+			<Label className='mb-4'>Links</Label>
+			<Input className='mb-8' placeholder='Website' />
+			<Button>Save</Button>
+		</div>
+	)
 }
 
 export default ProfileContent
