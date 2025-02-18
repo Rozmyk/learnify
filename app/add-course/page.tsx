79876e8ccc46a2e7d304b60467ec62f404302bad
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-18 11:28:44 +0100

    Add user auth check

--- app/add-course/page.tsx
+++ app/add-course/page.tsx
@@ -1,4 +1,18 @@
 import AddCourse from '@/components/AddCourse/AddCourse'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 export default async function page() {
+	const supabase = await createClient()
+
+	const {
+		data: { user },
+		error: authError,
+	} = await supabase.auth.getUser()
+	if (!user) {
+		redirect('/')
+	}
+	if (authError) {
+		console.log(authError)
+	}
 	return <AddCourse />
 }
