63bbecf8ae15c6b6c5a5638af12da1d6c567ee4b
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-19 12:51:13 +0100

    Change font weight

--- components/EditProfile/Content/DeleteContent/DeleteContent.tsx
+++ components/EditProfile/Content/DeleteContent/DeleteContent.tsx
@@ -3,9 +3,9 @@ const DeleteContent = () => {
 	return (
 		<div className='flex flex-col justify-start items-start w-full '>
 			<p className='mb-8'>
-				<span className='text-red-400'>Warning!</span> If you close your account, we will unsubscribe you from all
-				courses you attend and you will forever lose access to your account and associated data, even if you create a
-				new account using the same email address in the future.
+				<span className='text-red-400 font-semibold'>Warning!</span> If you close your account, we will unsubscribe you
+				from all courses you attend and you will forever lose access to your account and associated data, even if you
+				create a new account using the same email address in the future.
 			</p>
 			<Button>Delete account</Button>
 		</div>
