d8c2c538f2c0e4c3044188da0b585baedcf3bdcf
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-19 13:38:46 +0100

    Update ConfirmModal.tsx

--- components/EditProfile/Content/DeleteContent/ConfirmModal/ConfirmModal.tsx
+++ components/EditProfile/Content/DeleteContent/ConfirmModal/ConfirmModal.tsx
@@ -1,5 +1,31 @@
-const ConfirmModal = () => {
-	return <div></div>
+import * as Dialog from '@radix-ui/react-dialog'
+import { ReactNode } from 'react'
+import { Button } from '@/components/ui/button'
+const ConfirmModal = ({ children }: { children: ReactNode }) => {
+	return (
+		<Dialog.Root>
+			<Dialog.Trigger asChild>{children}</Dialog.Trigger>
+
+			<Dialog.Portal>
+				<Dialog.Overlay className='bg-black/50 fixed inset-0' />
+				<Dialog.Content className='bg-background border border-border rounded-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 shadow-lg w-96'>
+					<Dialog.Title className='text-lg font-bold'>Delete account</Dialog.Title>
+					<Dialog.Description className='text-sm text-muted-foreground mt-2'>
+						Are you sure you want to delete your account? This action cannot be undone.
+					</Dialog.Description>
+
+					<div className='mt-4 flex justify-end gap-2'>
+						<Dialog.Close asChild>
+							<Button>Close</Button>
+						</Dialog.Close>
+						<Dialog.Close asChild>
+							<Button>Confirm</Button>
+						</Dialog.Close>
+					</div>
+				</Dialog.Content>
+			</Dialog.Portal>
+		</Dialog.Root>
+	)
 }
 
 export default ConfirmModal
