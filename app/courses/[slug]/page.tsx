3c5333943bc43eb73f3968afdec98d4ea181dca7
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-20 12:27:49 +0100

    Create categories page

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -4,12 +4,10 @@ export default async function CoursesPage({ params }: { params: Promise<{ slug: 
 	const slug = (await params).slug
 	const supabase = await createClient()
 
-	const { data: category } = await supabase.from('categories').select('id').eq('slug', slug).single()
-
+	const { data: category } = await supabase.from('categories').select('*').eq('slug', slug).single()
 	if (!category) {
 		return <p>Category not found</p>
 	}
-	console.log(category)
 
 	const { data: courses } = await supabase.from('course').select('*').eq('categories_id', category.id)
 
@@ -18,10 +16,16 @@ export default async function CoursesPage({ params }: { params: Promise<{ slug: 
 	}
 
 	return (
-		<ul>
-			{courses.map(course => (
-				<li key={course.id}>{course.title}</li>
-			))}
-		</ul>
+		<>
+			<h1 className='text-4xl font-semibold mt-8'>Courses from the {category.name} category</h1>
+			<h2>Starting courses</h2>
+			<p>Discover courses created by experts with practical experience.</p>
+
+			<ul>
+				{courses.map(course => (
+					<li key={course.id}>{course.title}</li>
+				))}
+			</ul>
+		</>
 	)
 }
