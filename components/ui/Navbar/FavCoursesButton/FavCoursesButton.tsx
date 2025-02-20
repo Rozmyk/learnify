9aebd1ecdbddf1664fdb7893e20097498bf015da
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-20 18:27:15 +0100

    Create FavCoursesButton.tsx

---
+++ components/ui/Navbar/FavCoursesButton/FavCoursesButton.tsx
@@ -0,0 +1,12 @@
+import { HeaterIcon } from 'lucide-react'
+import { Button } from '../../button'
+
+const FavCoursesButton = () => {
+	return (
+		<Button>
+			<HeaterIcon />
+		</Button>
+	)
+}
+
+export default FavCoursesButton
