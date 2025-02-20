7b6a225c4c03b381670e406a4c0a71b76424b7da
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-20 19:20:46 +0100

    Add SingleFavCourse and button to redirect

--- components/ui/Navbar/FavCoursesButton/FavCoursesButton.tsx
+++ components/ui/Navbar/FavCoursesButton/FavCoursesButton.tsx
@@ -1,13 +1,8 @@
 import { Heart } from 'lucide-react'
 import { Button } from '../../button'
 import { fetchFavoriteCourses } from '@/lib/fetchFavouriteCourses'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuRadioGroup,
-	DropdownMenuRadioItem,
-	DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import SingleFavCourse from './SingleFavCourse/SingleFavCourse'
 
 const FavCoursesButton = async ({ userId }: { userId: string }) => {
 	const { data, error } = await fetchFavoriteCourses(userId)
@@ -18,10 +13,16 @@ const FavCoursesButton = async ({ userId }: { userId: string }) => {
 					<Heart className={'text-muted-foreground'} size={16} />
 				</Button>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent className='w-content' align='start'>
-				{data?.map(course => {
-					return <div>{course.title}</div>
-				})}
+			<DropdownMenuContent className='w-content ' align='start'>
+				<div className='p-4 flex flex-col justify-start items-start max-w-80 gap-4'>
+					{data?.map(course => {
+						return <SingleFavCourse {...course} />
+					})}
+				</div>
+				<div className='w-full border-t border-border my-2'></div>
+				<div className='w-full p-2'>
+					<Button className='w-full'>Go to wishlist</Button>
+				</div>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
