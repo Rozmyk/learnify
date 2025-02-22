7289a9270a4a7316b91d93f89e0e407084cfda9c
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-22 13:44:31 +0100

    Fix: fix type

--- context/cart.ts
+++ context/cart.ts
@@ -3,14 +3,14 @@
 import { create } from 'zustand'
 import { createClient } from '@/utils/supabase/client'
 import { CartItemProps } from '@/types/api'
-interface cartState {
+interface CartState {
 	cartItems: CartItemProps[]
 	loading: boolean
 	hasFetchedCartItems: boolean
 	fetchCart: (userId: string) => void
-	toggleCartIem: (userId: string) => void
+	toggleCartItem: (userId: string) => void
 }
-export const useCartStore = create((set, get) => ({
+export const useCartStore = create<CartState>((set, get) => ({
 	cartItems: [] as CartItemProps[],
 	loading: true,
 	hasFetchedCartItems: false,
