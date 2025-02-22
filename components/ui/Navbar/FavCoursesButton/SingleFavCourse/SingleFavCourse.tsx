73f2b635e15dfbd999e713a7c3918a3003e6428d
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-22 12:06:01 +0100

    Replace <div> to <Link> and href

--- components/ui/Navbar/FavCoursesButton/SingleFavCourse/SingleFavCourse.tsx
+++ components/ui/Navbar/FavCoursesButton/SingleFavCourse/SingleFavCourse.tsx
@@ -1,11 +1,12 @@
 import { CourseProps } from '@/types/api'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
-const SingleFavCourse = ({ title, profiles, thumbnail, price, discount }: CourseProps) => {
+const SingleFavCourse = ({ title, profiles, thumbnail, price, discount, slug }: CourseProps) => {
 	return (
 		<div className='flex flex-col justify-start items-start'>
-			<div className='flex justify-start items-center gap-4'>
+			<Link href={`/course/${slug}`} className='flex justify-start items-center gap-4'>
 				<div className='relative w-16 min-w-16 min-h-16 h-16 aspect-w-1 aspect-h-1 rounded-md overflow-hidden'>
 					<Image src={thumbnail} fill alt='course photo' className='object-cover' />
 				</div>
@@ -24,7 +25,7 @@ const SingleFavCourse = ({ title, profiles, thumbnail, price, discount }: Course
 						</p>
 					</div>
 				</div>
-			</div>
+			</Link>
 			<Button variant='outline' size='sm' className='w-full mt-2'>
 				Add to cart
 			</Button>
