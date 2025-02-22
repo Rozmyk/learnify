6fb5c16b2ddc2b660f668ea0f853af034c16bf44
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-22 13:42:23 +0100

    Update cart context

--- context/cart.ts
+++ context/cart.ts
@@ -2,14 +2,20 @@
 
 import { create } from 'zustand'
 import { createClient } from '@/utils/supabase/client'
-import { CourseProps } from '@/types/api'
-
+import { CartItemProps } from '@/types/api'
+interface cartState {
+	cartItems: CartItemProps[]
+	loading: boolean
+	hasFetchedCartItems: boolean
+	fetchCart: (userId: string) => void
+	toggleCartIem: (userId: string) => void
+}
 export const useCartStore = create((set, get) => ({
-	cartItems: [] as CourseProps[],
+	cartItems: [] as CartItemProps[],
 	loading: true,
 	hasFetchedCartItems: false,
 
 	fetchCart: async (userId: string) => {},
 
-	toggleCartItem: async (userId: string, courseId: string) => {},
+	toggleCartItem: async (userId: string) => {},
 }))
