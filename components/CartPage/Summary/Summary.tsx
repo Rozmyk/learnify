1152626e33302d004f23f9db28c8318e6aaface6
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-24 12:25:49 +0100

    Add discount price

--- components/CartPage/Summary/Summary.tsx
+++ components/CartPage/Summary/Summary.tsx
@@ -1,15 +1,21 @@
 import { Button } from '@/components/ui/button'
 import Promotions from '../Promotions/Promotions'
 import { useCartStore } from '@/context/cart'
+
 const Summary = ({ totalPrice }: { totalPrice: number }) => {
 	const { discount, originalTotal } = useCartStore()
+	const hasDiscount = discount !== 0 && originalTotal !== 0
 
 	return (
 		<>
 			<p className='text-muted-foreground text-lg font-semibold'>Summary:</p>
 			<p className='text-3xl font-semibold mb-2'>{totalPrice} zł</p>
-			{discount && <p className='text-muted-foreground  line-through'>{originalTotal} zł</p>}
-			{discount && <p className='text-muted-foreground  mb-2'>{discount} % discount</p>}
+			{hasDiscount && (
+				<>
+					<p className='text-muted-foreground line-through text-nowrap'>{originalTotal} zł</p>
+					<p className='text-muted-foreground mb-2 text-nowrap'>{discount} % discount</p>
+				</>
+			)}
 			<Button className='w-full mb-2'>Go to checkout</Button>
 			<p className='text-muted-foreground text-sm'>You won't pay anything yet</p>
 			<div className='border-b border-border my-4'></div>
