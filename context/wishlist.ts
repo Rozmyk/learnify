4e412e76779dd09268225a331cbce13f2149d1ac
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-24 11:55:40 +0100

    Remove userId from props in functions

--- context/wishlist.ts
+++ context/wishlist.ts
@@ -7,8 +7,8 @@ import { CourseProps } from '@/types/api'
 interface WishlistState {
 	favorites: CourseProps[]
 	loading: boolean
-	fetchFavorites: (userId: string) => Promise<void>
-	toggleFavorite: (userId: string, courseId: string) => Promise<void>
+	fetchFavorites: () => Promise<void>
+	toggleFavorite: (courseId: string) => Promise<void>
 	hasFetchedFavorites: boolean
 }
 
@@ -17,16 +17,23 @@ export const useWishlistStore = create<WishlistState>((set, get) => ({
 	loading: true,
 	hasFetchedFavorites: false,
 
-	fetchFavorites: async userId => {
+	fetchFavorites: async () => {
 		const { hasFetchedFavorites } = get()
 		if (hasFetchedFavorites) return
 
 		const supabase = createClient()
+		const {
+			data: { user },
+		} = await supabase.auth.getUser()
+		if (!user) {
+			set({ loading: false })
+			return
+		}
 
 		const { data: favCourses, error } = (await supabase
 			.from('favCourses')
 			.select('course(*, profiles(*))')
-			.eq('user_id', userId)) as { data: { course: CourseProps }[] | null; error: any }
+			.eq('user_id', user.id)) as { data: { course: CourseProps }[] | null; error: any }
 
 		if (error) {
 			console.error('Error fetching favorites:', error.message)
@@ -38,14 +45,20 @@ export const useWishlistStore = create<WishlistState>((set, get) => ({
 		set({ favorites: coursesWithFavorites || [], loading: false, hasFetchedFavorites: true })
 	},
 
-	toggleFavorite: async (userId, courseId) => {
+	toggleFavorite: async courseId => {
 		const supabase = createClient()
 		const { favorites } = get()
+		const {
+			data: { user },
+		} = await supabase.auth.getUser()
+		if (!user) {
+			return
+		}
 
 		const isFavorite = favorites.some(fav => fav.id === courseId)
 
 		if (isFavorite) {
-			const { error } = await supabase.from('favCourses').delete().eq('user_id', userId).eq('course_id', courseId)
+			const { error } = await supabase.from('favCourses').delete().eq('user_id', user.id).eq('course_id', courseId)
 
 			if (error) {
 				console.error('Error removing favorite:', error.message)
@@ -54,7 +67,7 @@ export const useWishlistStore = create<WishlistState>((set, get) => ({
 
 			set({ favorites: favorites.filter(fav => fav.id !== courseId) })
 		} else {
-			const { error } = await supabase.from('favCourses').insert([{ user_id: userId, course_id: courseId }])
+			const { error } = await supabase.from('favCourses').insert([{ user_id: user.id, course_id: courseId }])
 
 			if (error) {
 				console.error('Error adding favorite:', error.message)
