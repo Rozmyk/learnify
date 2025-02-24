888420c6433ca2e54713926b395ce0b70fb8aea0
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-24 12:20:29 +0100

    Fetch price without discount from cart context

--- components/CartPage/Summary/Summary.tsx
+++ components/CartPage/Summary/Summary.tsx
@@ -1,10 +1,15 @@
 import { Button } from '@/components/ui/button'
 import Promotions from '../Promotions/Promotions'
+import { useCartStore } from '@/context/cart'
 const Summary = ({ totalPrice }: { totalPrice: number }) => {
+	const { discount, originalTotal } = useCartStore()
+
 	return (
 		<>
 			<p className='text-muted-foreground text-lg font-semibold'>Summary:</p>
 			<p className='text-3xl font-semibold mb-2'>{totalPrice} zł</p>
+			{discount && <p className='text-muted-foreground  line-through'>{originalTotal} zł</p>}
+			{discount && <p className='text-muted-foreground  mb-2'>{discount} % discount</p>}
 			<Button className='w-full mb-2'>Go to checkout</Button>
 			<p className='text-muted-foreground text-sm'>You won't pay anything yet</p>
 			<div className='border-b border-border my-4'></div>
