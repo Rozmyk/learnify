163230a5f6af517fce63291e6af168eeec32d0f2
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-24 11:57:11 +0100

    Add remove from cart and add to wishlist functions

--- components/CartPage/CartPage.tsx
+++ components/CartPage/CartPage.tsx
@@ -4,25 +4,39 @@ import Loader from '../ui/loader'
 import EmptyCart from './EmptyCart/EmptyCart'
 import SingleCartItem from './SingleCartItem/SingleCartItem'
 import Summary from './Summary/Summary'
-
+import LastWishlistItem from './LastWishlistItem/LastWishlistItem'
+import { useWishlistStore } from '@/context/wishlist'
 const CartPage = () => {
-	const { cartItems, totalPrice, loading } = useCartStore()
+	const { cartItems, totalPrice, loading, removeFromCart } = useCartStore()
+	const { toggleFavorite } = useWishlistStore()
 
 	return loading ? (
-		<Loader />
+		<div className='w-full h-96 flex justify-center items-center'>
+			<Loader />
+		</div>
 	) : (
 		<div className='p-4'>
 			<h1 className='text-3xl font-semibold mb-4'>Cart</h1>
 			<div className='flex w-full flex-col md:flex-row justify-between items-start gap-8'>
-				{cartItems.length > 0 ? (
+				{cartItems && cartItems.length > 0 ? (
 					<>
 						<div className='w-full md:w-2/3'>
 							<div className='border-b border-border w-full mb-4'>
 								<p className='py-2 font-semibold'>{cartItems.length} courses in cart</p>
 							</div>
 							{cartItems.map(item => (
-								<SingleCartItem key={item.id} {...item} />
+								<SingleCartItem
+									onDeleteClick={() => {
+										removeFromCart(item.product_id)
+									}}
+									moveToWishlist={() => {
+										toggleFavorite(item.product_id)
+									}}
+									key={item.id}
+									{...item}
+								/>
 							))}
+							<LastWishlistItem />
 						</div>
 						<div className='w-full md:w-1/3'>
 							<Summary totalPrice={totalPrice} />
