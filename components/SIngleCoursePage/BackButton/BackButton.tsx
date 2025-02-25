7f658b81f755e62840d776052dfd825ce61be780
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-25 19:58:22 +0100

    Update styles

--- components/SIngleCoursePage/BackButton/BackButton.tsx
+++ components/SIngleCoursePage/BackButton/BackButton.tsx
@@ -2,7 +2,7 @@ import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 const BackButton = () => {
 	return (
-		<Link className='absolute top-2 left-2 z-20 cursor-pointer font-semibold flex gap-2' href='/'>
+		<Link className='absolute top-4 left-4 z-20 cursor-pointer font-semibold flex gap-2' href='/'>
 			<ArrowLeft />
 			Back to course
 		</Link>
