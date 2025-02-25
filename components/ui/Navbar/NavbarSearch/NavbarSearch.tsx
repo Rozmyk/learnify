ff64de4b215ae9385e4d8c215bfbb6c14aff9801
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-25 15:31:07 +0100

    Create NavbarSearch.tsx

---
+++ components/ui/Navbar/NavbarSearch/NavbarSearch.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { Input } from '../../input'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { CourseProps } from '@/types/api'
+import Image from 'next/image'
+const SingleSearchCourse = ({ course }: { course: CourseProps }) => {
+	return (
+		<div className='flex justify-start items-start gap-4'>
+			<div className='w-10 h-10 relative'>
+				<Image src={course.thumbnail} alt='course image' className='object-cover' fill />
+			</div>
+			<div className='flex flex-col'>
+				<h3 className='font-semibold '>{course.title}</h3>
+				<div className='flex justify-start items-center gap-2 '>
+					<p className='font-semibold text-sm text-muted-foreground'>Course:</p>
+					<p className='text-sm text-muted-foreground'>{course.profiles.username}</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+const NavbarSearch = () => {
+	const [inputValue, setInputValue] = useState('')
+	const [courses, setCourses] = useState([])
+
+	useEffect(() => {
+		const fetchCourses = async () => {
+			if (inputValue.trim().length < 2) {
+				setCourses([])
+				return
+			}
+
+			const res = await fetch(`/api/course/search?q=${inputValue}`)
+			const data = await res.json()
+			setCourses(data.courses)
+		}
+
+		const debounce = setTimeout(fetchCourses, 300)
+
+		return () => clearTimeout(debounce)
+	}, [inputValue])
+
+	return (
+		<div className='flex-1 relative'>
+			<Input
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+				type='text'
+				placeholder='Search for any courses...'
+				className='w-full rounded-xl'
+			/>
+			{inputValue.trim().length >= 3 && courses.length > 0 && (
+				<div className='w-full absolute top-full left-0 bg-black border border-border shadow-lg p-2 mt-2 rounded-md'>
+					<ul>
+						{courses.map((course: CourseProps) => (
+							<Link
+								href={`/course/${course.slug}`}
+								onClick={() => {
+									setInputValue('')
+								}}>
+								<SingleSearchCourse course={course} />
+							</Link>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default NavbarSearch
