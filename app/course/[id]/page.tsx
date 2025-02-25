6e5b35e3659d2bbfe0d2cf867c84ea055c2d45c1
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-25 18:32:17 +0100

    Add function to fetch course data

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -1,4 +1,23 @@
+import SingleCoursePage from '@/components/SIngleCoursePage/SingleCoursePage'
+import { createClient } from '@/utils/supabase/server'
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
 	const id = (await params).id
-	return <div>My Post: {id}</div>
+	const supabase = await createClient()
+	const { data, error } = await supabase
+		.from('course')
+		.select(
+			`
+				  *,
+				  reviews(*),
+				  categories(*),
+				  profiles(*)
+				`
+		)
+		.eq('slug', id)
+		.single()
+
+	if (error) {
+		console.log(error.message)
+	}
+	return <SingleCoursePage course={data} />
 }
