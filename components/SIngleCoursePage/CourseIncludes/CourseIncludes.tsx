b83e7d74abfd56289552073a71250050f9798b32
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-26 21:38:08 +0100

    Add Icons

--- components/SIngleCoursePage/CourseIncludes/CourseIncludes.tsx
+++ components/SIngleCoursePage/CourseIncludes/CourseIncludes.tsx
@@ -1,11 +1,16 @@
-import { MonitorPlay, NotepadText } from 'lucide-react'
+import { MonitorPlay, NotepadText, Trophy, Smartphone, Download } from 'lucide-react'
 import SingleFeature from './SingleFeature/SingleFeature'
 const CourseIncludes = () => {
 	return (
 		<div>
 			<h3 className='text-2xl font-semibold mb-4'>This course includes:</h3>
-			<SingleFeature Icon={MonitorPlay} value='Zadania' />
-			<SingleFeature Icon={NotepadText} value='Zadania' />
+			<div className='grid grid-cols-2 gap-4'>
+				<SingleFeature Icon={MonitorPlay} value='20.5 hours of video-on-demand content' />
+				<SingleFeature Icon={NotepadText} value='Tasks' />
+				<SingleFeature Icon={Download} value='Downloadable resources: 105' />
+				<SingleFeature Icon={Smartphone} value='Access on mobile devices and TVs' />
+				<SingleFeature Icon={Trophy} value='Certificate of completion' />
+			</div>
 		</div>
 	)
 }
