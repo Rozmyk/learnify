b66c0e4ec2052e3f35b3f258d282fa0f630a4320
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-26 21:38:21 +0100

    Change styles and add icons

--- components/SIngleCoursePage/CourseHeader/CourseHeader.tsx
+++ components/SIngleCoursePage/CourseHeader/CourseHeader.tsx
@@ -2,6 +2,7 @@ import formatTimestamp from '@/lib/formatTimestamp'
 import StarRating from '@/components/ui/starRating'
 import { ProfileDataProps, ReviewProps } from '@/types/api'
 import Link from 'next/link'
+import { Earth, CircleAlert } from 'lucide-react'
 const CourseHeader = ({
 	title,
 	description,
@@ -18,15 +19,24 @@ const CourseHeader = ({
 	return (
 		<>
 			<h1 className='text-4xl font-semibold mb-2'>{title}</h1>
-			<p>{description}</p>
+			<p className='mb-2'>{description}</p>
 			<StarRating reviews={reviews} />
-			<p className='text-muted-foreground text-sm'>
+			<p className='text-muted-foreground text-sm my-2'>
 				Created by:{' '}
 				<Link className='font-semibold text-primary' href={`/user/${profiles.username}`}>
 					{profiles.username}
 				</Link>
 			</p>
-			<p className='text-sm'>Last updated: {formatTimestamp(created_at)} </p>
+			<div className='flex justify-start items-center gap-2'>
+				<div className='flex justify-start items-center gap-2'>
+					<CircleAlert size={16} />
+					<p className='text-sm'>Last updated: {formatTimestamp(created_at)} </p>
+				</div>
+				<div className='flex justify-start items-center gap-2'>
+					<Earth size={16} />
+					<p className='text-sm'>Language: English </p>
+				</div>
+			</div>
 		</>
 	)
 }
