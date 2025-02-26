e7075939cdad5a29021a944e7ea6b48cc2b9e437
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-26 18:33:18 +0100

    Create WhatYouLearn.tsx

---
+++ components/SIngleCoursePage/WhatYouLearn/WhatYouLearn.tsx
@@ -0,0 +1,23 @@
+import SingleLessonPoint from './SingleLessonPoint/SingleLessonPoint'
+const WhatYouLearn = ({ skills_gained }: { skills_gained: string }) => {
+	const splitStringToArray = (text: string) => {
+		if (!text) return []
+		return text
+			.split(';')
+			.map(sentence => sentence.trim())
+			.filter(sentence => sentence.length > 0)
+	}
+	const skillsArray = splitStringToArray(skills_gained)
+	return (
+		<div className='mt-10 border border-border p-4  w-full md:w-2/3 rounded-lg '>
+			<h3 className='text-2xl font-semibold mb-8'>What you will learn</h3>
+			<div className='grid grid-cols-2 gap-4'>
+				{skillsArray.map(singleText => (
+					<SingleLessonPoint key={singleText} value={singleText} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default WhatYouLearn
