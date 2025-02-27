a6f0bdcfd0f4beff60a563e4e92e72d99e8ca9da
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-27 16:36:32 +0100

    Change padding

--- components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
+++ components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
@@ -1,6 +1,6 @@
 const ReviewsHeader = ({ avgRating, reviewCount }: { avgRating: number | null; reviewCount: number }) => {
 	return (
-		<div className='flex justify-start items-center gap-2  py-8 mb-2'>
+		<div className='flex justify-start items-center gap-2  py-2 mb-2'>
 			<span className='text-orange-400 text-3xl'>★</span>
 			<p className='font-semibold text-2xl'>{avgRating} course rating</p>
 			<span className='text-2xl text-muted-foreground'>&#8226;</span>
