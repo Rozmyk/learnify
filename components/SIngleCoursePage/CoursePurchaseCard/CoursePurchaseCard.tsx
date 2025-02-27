332f77d445ccea939a8d78e6f628c0b485173683
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-27 16:48:49 +0100

    Create CoursePurchaseCard.tsx

---
+++ components/SIngleCoursePage/CoursePurchaseCard/CoursePurchaseCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import Promotions from '@/components/CartPage/Promotions/Promotions'
+import FavButton from '@/components/FavButton/FavButton'
+import { CourseProps } from '@/types/api'
+const CoursePurchaseCard = ({ thumbnail, discount, price, id }: CourseProps) => {
+	return (
+		<div className='md:sticky relative md:top-0 md:right-0 bg-background border border-border opacity-95 rounded-lg shadow-lg  '>
+			<div className='w-full h-44 relative mb-4 rounded-lg overflow-hidden'>
+				<Image fill src={thumbnail} alt='course photo' />
+			</div>
+			<div className='w-full p-4'>
+				<div className='flex gap-2 justify-start items-center mb-4'>
+					<p className='font-semibold text-2xl'>{price} zł</p>
+					{discount && <p className='text-muted-foreground'>-{discount} % discount</p>}
+				</div>
+
+				<div className='flex gap-2 mb-4'>
+					<Button className='w-full'>Add to cart</Button>
+					<div>
+						<FavButton variant='default' courseId={id} />
+					</div>
+				</div>
+				<Button className='w-full'>Buy now</Button>
+				<p className='text-sm text-center my-2 text-muted-foreground'>30-day money back guarantee</p>
+				<Button variant='link'>Share</Button>
+				<Promotions withoutText />
+			</div>
+		</div>
+	)
+}
+
+export default CoursePurchaseCard
