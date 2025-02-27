c4bb232690b39d3436ca939542cae90b1a71dc89
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-27 15:25:09 +0100

    Create SingleReview.tsx

---
+++ components/SIngleCoursePage/ReviewsCourse/SingleReview/SingleReview.tsx
@@ -0,0 +1,22 @@
+import { ReviewProps } from '@/types/api'
+import Image from 'next/image'
+import timeAgo from '@/lib/timeAgo'
+
+const SingleReview = ({ review }: { review: ReviewProps }) => {
+	return (
+		<div className=' flex flex-col justify-start items-start gap-4 border-t border-border p-2'>
+			<div className='flex justify-start items-center gap-2 '>
+				<div className='w-12 h-12 min-h-12 min-w-12 relative rounded-full overflow-hidden'>
+					<Image fill alt='user photo' src={review.profiles.avatar_url} />
+				</div>
+				<div className='flex flex-col justify-start items-start text-sm'>
+					<p className='font-semibold'>{review.profiles.username}</p>
+					<p className='text-muted-foreground'>{timeAgo(review.created_at)}</p>
+				</div>
+			</div>
+			<p>{review.content}</p>
+		</div>
+	)
+}
+
+export default SingleReview
