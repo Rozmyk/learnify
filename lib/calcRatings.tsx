5816bdbce493935f754a714022da0f59654bfdb2
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-28 14:44:30 +0100

    Change default value when reviews dosent exist

--- lib/calcRatings.tsx
+++ lib/calcRatings.tsx
@@ -1,7 +1,7 @@
-import { CourseProps, ReviewProps } from '@/types/api'
+import { ReviewProps } from '@/types/api'
 
 export function addRatingsToCourses(reviews: ReviewProps[]) {
 	const reviewCount = reviews.length
-	const avgRating = reviewCount > 0 ? reviews.reduce((sum, review) => sum + review.rating, 0) / reviewCount : null
+	const avgRating = reviewCount > 0 ? reviews.reduce((sum, review) => sum + review.rating, 0) / reviewCount : 0
 	return { avgRating, reviewCount }
 }
