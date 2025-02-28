1b42d877458d0b7ea6680003b1a8e8eb16e7937e
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-28 12:25:43 +0100

    Change text and icon size

--- components/SIngleCoursePage/BackButton/BackButton.tsx
+++ components/SIngleCoursePage/BackButton/BackButton.tsx
@@ -2,9 +2,11 @@ import { ArrowLeft } from 'lucide-react'
 import Link from 'next/link'
 const BackButton = () => {
 	return (
-		<Link className='absolute top-4 left-4 z-20 cursor-pointer font-semibold flex gap-2' href='/'>
-			<ArrowLeft />
-			Back to course
+		<Link
+			className='absolute top-4 left-4 z-20 cursor-pointer font-semibold text-sm flex justify-start items-center gap-2'
+			href='/'>
+			<ArrowLeft size={16} />
+			Back to courses
 		</Link>
 	)
 }
