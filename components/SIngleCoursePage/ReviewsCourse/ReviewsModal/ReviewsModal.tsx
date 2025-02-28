e79737310e9522ef2dced142ae6cb1d99e770d0e
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-28 14:46:54 +0100

    Wrap div by Dialog.Title

--- components/SIngleCoursePage/ReviewsCourse/ReviewsModal/ReviewsModal.tsx
+++ components/SIngleCoursePage/ReviewsCourse/ReviewsModal/ReviewsModal.tsx
@@ -25,12 +25,14 @@ const ReviewsModal = ({ children, reviews }: { children: ReactNode; reviews: Rev
 			<Dialog.Portal>
 				<Dialog.Overlay className='bg-black bg-opacity-60 fixed inset-0 z-40' />
 				<Dialog.Content className='fixed top-1/2 left-1/2 w-full max-w-3xl bg-secondary rounded-lg shadow-lg flex flex-col justify-start items-start z-50 transform -translate-x-1/2 -translate-y-1/2'>
-					<div className='flex justify-between items-center w-full p-4'>
-						<ReviewsHeader avgRating={reviewsData.avgRating} reviewCount={reviewsData.reviewCount} />
-						<Button variant='secondary' onClick={handleClose} className='rounded-full ' size='icon'>
-							<X />
-						</Button>
-					</div>
+					<Dialog.Title className='w-full'>
+						<div className='flex justify-between items-center w-full p-4'>
+							<ReviewsHeader avgRating={reviewsData.avgRating} reviewCount={reviewsData.reviewCount} />
+							<Button variant='secondary' onClick={handleClose} className='rounded-full ' size='icon'>
+								<X />
+							</Button>
+						</div>
+					</Dialog.Title>
 					<div className='flex flex-col justify-start items-center p-4 gap-8'>
 						{reviews.map(review => {
 							return <SingleReview review={review} />
