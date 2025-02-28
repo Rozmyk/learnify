8b7559138fc71985d2966ed12a96ef6426bc6e8b
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-28 14:37:31 +0100

    Add Carousel for mobile devices

--- components/SIngleCoursePage/ReviewsCourse/ReviewsCourse.tsx
+++ components/SIngleCoursePage/ReviewsCourse/ReviewsCourse.tsx
@@ -4,17 +4,23 @@ import SingleReview from './SingleReview/SingleReview'
 import { Button } from '@/components/ui/button'
 import ReviewsModal from './ReviewsModal/ReviewsModal'
 import ReviewsHeader from './ReviewsHeader/ReviewsHeader'
+import Carousel from './Carousel/Carousel'
 
 const ReviewsCourse = ({ reviews }: { reviews: ReviewProps[] }) => {
 	const reviewData = addRatingsToCourses(reviews)
 	const limitedReviews = reviews.slice(0, 4)
 	return (
-		<div className=' mb-20'>
+		<div className=' mb-20 max-w-full'>
 			<ReviewsHeader reviewCount={reviewData.reviewCount} avgRating={reviewData.avgRating} />
-			<div className='grid grid-cols-2 gap-8 p-2'>
-				{limitedReviews.map(review => {
-					return <SingleReview key={review.id} review={review} />
-				})}
+			<div className='hidden md:block'>
+				<div className='grid grid-cols-2 gap-8 p-2 '>
+					{limitedReviews.map(review => {
+						return <SingleReview key={review.id} review={review} />
+					})}
+				</div>
+			</div>
+			<div className='block md:hidden'>
+				<Carousel reviews={limitedReviews} />
 			</div>
 
 			{reviews.length > 4 && (
