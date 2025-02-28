7efb90edf4491442cca626550a8936da220e1700
Michał Rozmysłowski <kymzor@gmail.com>
2025-02-28 12:24:17 +0100

    Add toFixed to avgRating

--- components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
+++ components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
@@ -2,7 +2,7 @@ const ReviewsHeader = ({ avgRating, reviewCount }: { avgRating: number | null; r
 	return (
 		<div className='flex justify-start items-center gap-2  py-2 mb-2'>
 			<span className='text-orange-400 text-3xl'>★</span>
-			<p className='font-semibold text-2xl'>{avgRating} course rating</p>
+			<p className='font-semibold text-2xl'>{avgRating?.toFixed(1)} course rating</p>
 			<span className='text-2xl text-muted-foreground'>&#8226;</span>
 			<p className='font-semibold text-2xl'>{reviewCount} ratings</p>
 		</div>
