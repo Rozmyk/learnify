3a4ed89bbc09927040431e47ff9b0c85a9790bc3
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-01 19:28:45 +0100

    Add buton to redirect  to my courses

--- components/ui/Navbar/MyCoursesButton/MyCoursesButton.tsx
+++ components/ui/Navbar/MyCoursesButton/MyCoursesButton.tsx
@@ -28,19 +28,25 @@ const MyCoursesButton = () => {
 						<Loader />
 					</div>
 				) : (
-					<ScrollArea.Root>
-						<ScrollArea.Viewport style={{ maxHeight: 500 }}>
-							<ScrollArea.Scrollbar orientation='vertical'>
-								<ScrollArea.Thumb />
-							</ScrollArea.Scrollbar>
-							<div className='p-4 flex flex-col justify-start items-start max-w-80 gap-4'>
-								{owned.map(item => {
-									return <SingleOwnedCourse {...item.course} key={item.course.id} />
-								})}
-							</div>
-						</ScrollArea.Viewport>
-					</ScrollArea.Root>
+					<>
+						<ScrollArea.Root>
+							<ScrollArea.Viewport style={{ maxHeight: 500 }}>
+								<ScrollArea.Scrollbar orientation='vertical'>
+									<ScrollArea.Thumb />
+								</ScrollArea.Scrollbar>
+								<div className='p-4 flex flex-col justify-start items-start max-w-80 gap-4'>
+									{owned.map(item => {
+										return <SingleOwnedCourse {...item.course} key={item.course.id} />
+									})}
+								</div>
+							</ScrollArea.Viewport>
+						</ScrollArea.Root>
+						<div className='w-full border-t border-border my-2'></div>
+					</>
 				)}
+				<div className='w-full p-2 flex flex-col justify-center items-start gap-2'>
+					<Button className='w-full'>Open my courses</Button>
+				</div>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
