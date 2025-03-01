e47e5e367d55ebba3a80a9fbf69033747ad84370
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-01 17:58:01 +0100

    Add functon to fetch all owned courses

--- context/ownedCourses.ts
+++ context/ownedCourses.ts
@@ -2,19 +2,43 @@
 
 import { create } from 'zustand'
 import { createClient } from '@/utils/supabase/client'
-import { CourseProps } from '@/types/api'
+import { CourseProps, ownedCourseProps } from '@/types/api'
 
 interface OwnedCoursesState {
-	owned: CourseProps[]
+	owned: ownedCourseProps[]
 	loading: boolean
 	fetchOwned: () => Promise<void>
 	hasFetchedOwnedCourses: boolean
 }
 
 export const useOwnedCoursesStore = create<OwnedCoursesState>((set, get) => ({
-	owned: [] as CourseProps[],
+	owned: [],
 	loading: true,
 	hasFetchedOwnedCourses: false,
 
-	fetchOwned: async () => {},
+	fetchOwned: async () => {
+		const { hasFetchedOwnedCourses } = get()
+		if (hasFetchedOwnedCourses) return
+
+		const supabase = await createClient()
+		const {
+			data: { user },
+		} = await supabase.auth.getUser()
+		if (!user) {
+			set({ loading: false })
+			return
+		}
+
+		const { data, error } = await supabase.from('owned_courses').select('*, course(*)').eq('user_id', user.id)
+
+		if (error) {
+			console.log(error)
+			return
+		}
+		console.log(data)
+		set({
+			owned: data,
+			loading: false,
+		})
+	},
 }))
