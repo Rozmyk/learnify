dc8b70b2ff2dfe4ba4af100c9b4ef09b55efc7a4
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-01 17:58:15 +0100

    Add functions to remove all items in cart

--- context/cart.ts
+++ context/cart.ts
@@ -21,6 +21,7 @@ interface CartState {
 	removeFromCart: (productId: string) => Promise<void>
 	applyPromoCode: (code: string) => Promise<void>
 	calculateTotalPrice: () => void
+	removeAllItems: () => void
 }
 
 export const useCartStore = create<CartState>((set, get) => ({
@@ -33,6 +34,21 @@ export const useCartStore = create<CartState>((set, get) => ({
 	hasFetchedCartItems: false,
 	promoCode: null,
 	discount: 0,
+	removeAllItems: async () => {
+		const supabase = createClient()
+		const {
+			data: { user },
+		} = await supabase.auth.getUser()
+
+		if (!user?.id) {
+			return
+		}
+
+		const { error } = await supabase.from('cart').delete().eq('user_id', user.id)
+		if (!error) {
+			set({ cartItems: [], totalPrice: 0, originalTotal: 0, promoCode: null, discount: 0 })
+		}
+	},
 	setPromoError: (error: string | null) => set({ promoError: error }),
 	calculateTotalPrice: () => {
 		const { cartItems, discount } = get()
