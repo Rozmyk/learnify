da6ecd6b4aa68fa6a561178701adaf488388cbc1
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-02 17:19:18 +0100

    Add SingleCategoryPage

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { createClient } from '@/utils/supabase/server'
+import SingleCategoryPage from '@/components/SingleCategoryPage/SingleCategoryPage'
 
 export default async function CoursesPage({ params }: { params: Promise<{ slug: string }> }) {
 	const slug = (await params).slug
@@ -9,23 +10,14 @@ export default async function CoursesPage({ params }: { params: Promise<{ slug: 
 		return <p>Category not found</p>
 	}
 
-	const { data: courses } = await supabase.from('course').select('*').eq('categories_id', category.id)
+	const { data: courses } = await supabase
+		.from('course')
+		.select('*, profiles(*), categories(*), reviews(*)')
+		.eq('categories_id', category.id)
 
 	if (!courses || courses.length === 0) {
 		return <p>No courses found for this category.</p>
 	}
 
-	return (
-		<>
-			<h1 className='text-4xl font-semibold mt-8'>Courses from the {category.name} category</h1>
-			<h2>Starting courses</h2>
-			<p>Discover courses created by experts with practical experience.</p>
-
-			<ul>
-				{courses.map(course => (
-					<li key={course.id}>{course.title}</li>
-				))}
-			</ul>
-		</>
-	)
+	return <SingleCategoryPage courses={courses} category={category} slug={slug} />
 }
