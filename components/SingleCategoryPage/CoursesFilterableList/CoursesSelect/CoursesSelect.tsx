0a396882b7d7ef258ab4dffe0aa87f03f5c80171
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-03 13:14:17 +0100

    Create CoursesSelect.tsx

---
+++ components/SingleCategoryPage/CoursesFilterableList/CoursesSelect/CoursesSelect.tsx
@@ -0,0 +1,46 @@
+import * as Select from '@radix-ui/react-select'
+import { ChevronDown, Check } from 'lucide-react'
+const CoursesSelect = () => {
+	const selectArray = [
+		{
+			value: 'Highest rated',
+		},
+		{ value: 'Latest' },
+		{ value: 'Most popular' },
+	]
+	return (
+		<Select.Root>
+			<Select.Trigger className='inline-flex items-center justify-between bg-background border border-input  rounded-md px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 text-muted-foreground '>
+				<Select.Value placeholder='Choose category' />
+				<Select.Icon>
+					<ChevronDown />
+				</Select.Icon>
+			</Select.Trigger>
+
+			<Select.Portal>
+				<Select.Content className='bg-background border border-input rounded shadow-lg '>
+					<Select.ScrollUpButton />
+					<Select.Viewport className='p-2'>
+						{selectArray.map(item => {
+							return (
+								<Select.Item
+									key={item.value}
+									value={item.value}
+									className='cursor-pointer select-none px-4 py-2 rounded hover:bg-accent focus:bg-accent
+                flex items-center justify-between'>
+									<Select.ItemText>{item.value}</Select.ItemText>
+									<Select.ItemIndicator>
+										<Check />
+									</Select.ItemIndicator>
+								</Select.Item>
+							)
+						})}
+					</Select.Viewport>
+					<Select.ScrollDownButton />
+				</Select.Content>
+			</Select.Portal>
+		</Select.Root>
+	)
+}
+
+export default CoursesSelect
