5b2b63c43d99250ec8c826776c2f31a7eb18dc97
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-03 13:14:54 +0100

    Set up default value

--- components/SingleCategoryPage/CoursesFilterableList/FilterAccordion/FilterAccordion.tsx
+++ components/SingleCategoryPage/CoursesFilterableList/FilterAccordion/FilterAccordion.tsx
@@ -3,8 +3,14 @@ import SingleAccordion from './SingleAccordion/SingleAccordion'
 
 const FilterAccordion = () => {
 	return (
-		<Accordion.Root type='multiple' className='w-full'>
+		<Accordion.Root type='multiple' defaultValue={['Ranking', 'Price']} className='w-full border-t border-border'>
 			<SingleAccordion item='Ranking'>
+				<div>options</div>
+			</SingleAccordion>
+			<SingleAccordion item='Price'>
+				<div>options</div>
+			</SingleAccordion>
+			<SingleAccordion item='Level'>
 				<div>options</div>
 			</SingleAccordion>
 		</Accordion.Root>
