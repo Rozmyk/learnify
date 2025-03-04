2e3503b25f697e76fe1a9d5dbb4b2a99c29cf93f
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-04 19:35:44 +0100

    Split component into SingleSearchCourse

---
+++ components/SingleSearchCourse/SingleSearchCourse.tsx
@@ -0,0 +1,21 @@
+import { CourseProps } from '@/types/api'
+import Image from 'next/image'
+
+const SingleSearchCourse = ({ course }: { course: CourseProps }) => {
+	return (
+		<div className='flex justify-start items-start gap-4 mb-4'>
+			<div className='w-10 h-10 relative'>
+				<Image src={course.thumbnail} alt='course image' className='object-cover' fill />
+			</div>
+			<div className='flex flex-col'>
+				<h3 className='font-semibold '>{course.title}</h3>
+				<div className='flex justify-start items-center gap-2 '>
+					<p className='font-semibold text-sm text-muted-foreground'>Course:</p>
+					<p className='text-sm text-muted-foreground'>{course.profiles.username}</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default SingleSearchCourse
--- components/ui/Navbar/NavbarSearch/NavbarSearch.tsx
+++ components/ui/Navbar/NavbarSearch/NavbarSearch.tsx
@@ -4,23 +4,7 @@ import { Input } from '../../input'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { CourseProps } from '@/types/api'
-import Image from 'next/image'
-const SingleSearchCourse = ({ course }: { course: CourseProps }) => {
-	return (
-		<div className='flex justify-start items-start gap-4 mb-4'>
-			<div className='w-10 h-10 relative'>
-				<Image src={course.thumbnail} alt='course image' className='object-cover' fill />
-			</div>
-			<div className='flex flex-col'>
-				<h3 className='font-semibold '>{course.title}</h3>
-				<div className='flex justify-start items-center gap-2 '>
-					<p className='font-semibold text-sm text-muted-foreground'>Course:</p>
-					<p className='text-sm text-muted-foreground'>{course.profiles.username}</p>
-				</div>
-			</div>
-		</div>
-	)
-}
+import SingleSearchCourse from '@/components/SingleSearchCourse/SingleSearchCourse'
 
 const NavbarSearch = () => {
 	const [inputValue, setInputValue] = useState('')
@@ -49,9 +33,6 @@ const NavbarSearch = () => {
 			<Input
 				onFocus={() => {
 					setInputFocused(true)
-				}}
-				onBlur={() => {
-					setInputFocused(false)
 				}}
 				value={inputValue}
 				onChange={e => setInputValue(e.target.value)}
