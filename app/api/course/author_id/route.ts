f0da6c781550f2bda6f7aa27c93930a8bd7d664d
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-04 17:02:45 +0100

    Add limit props

--- app/api/course/author_id/route.ts
+++ app/api/course/author_id/route.ts
@@ -5,16 +5,19 @@ export async function GET(req: Request) {
 	const supabase = await createClient()
 	const { searchParams } = new URL(req.url)
 	const author_id = searchParams.get('author_id')
+	const limit = searchParams.get('limit')
 
 	if (!author_id) {
 		return NextResponse.json({ courses: [] })
 	}
 
-	const { data, error } = await supabase
-		.from('course')
-		.select('*, categories(*), profiles(*), reviews(*)')
-		.eq('author_id', author_id)
-		.limit(2)
+	let query = supabase.from('course').select('*, categories(*), profiles(*), reviews(*)').eq('author_id', author_id)
+
+	if (limit) {
+		query = query.limit(Number(limit))
+	}
+
+	const { data, error } = await query
 
 	if (error) {
 		return NextResponse.json({ error: error.message }, { status: 500 })
