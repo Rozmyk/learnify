92ffba450222d9392cc905bba7ad75b1a0d776df
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-04 19:55:02 +0100

    Add input logic and render SingleSearchCourse

--- components/ui/Navbar/NavbarMobile/SearchOverlay/SearchOverlay.tsx
+++ components/ui/Navbar/NavbarMobile/SearchOverlay/SearchOverlay.tsx
@@ -5,6 +5,8 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { CourseProps } from '@/types/api'
 import Loader from '@/components/ui/loader'
+import SingleSearchCourse from '@/components/SingleSearchCourse/SingleSearchCourse'
+import useDebouncedSearch from '@/lib/useDebouncedSearch'
 
 interface SearchOverlayProps {
 	handleClose: () => void
@@ -12,7 +14,8 @@ interface SearchOverlayProps {
 
 const SearchOverlay = ({ handleClose }: SearchOverlayProps) => {
 	const [inputValue, setInputValue] = useState('')
-	const [courses, setCourses] = useState<CourseProps | null>(null)
+
+	const { courses, loading } = useDebouncedSearch(inputValue)
 	useEffect(() => {
 		const handleEsc = (e: KeyboardEvent) => {
 			if (e.key === 'Escape') {
@@ -27,13 +30,29 @@ const SearchOverlay = ({ handleClose }: SearchOverlayProps) => {
 		<div className='fixed inset-0 bg-card z-50 flex flex-col '>
 			<div className='flex items-center justify-center border-b border-border pb-2 gap-2 p-4'>
 				<Search className='mr-2' />
-				<Input placeholder='Search for any courses...' />
+				<Input
+					value={inputValue}
+					onChange={e => {
+						setInputValue(e.target.value)
+					}}
+					placeholder='Search for any courses...'
+				/>
 				<Button size='icon' variant='ghost' onClick={handleClose}>
 					<X />
 				</Button>
 			</div>
 
-			<div className='flex-1  p-4 text-muted-foreground'>Here will be the search results...</div>
+			{loading ? (
+				<div className='flex justify-center items-center py-8'>
+					<Loader />
+				</div>
+			) : (
+				<div className='flex flex-col p-4'>
+					{courses?.map(course => {
+						return <SingleSearchCourse course={course} />
+					})}
+				</div>
+			)}
 		</div>
 	)
 }
