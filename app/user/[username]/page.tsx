c0888a7406165a8ca933127dde6ac7e0c6d39e9b
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-04 16:04:46 +0100

    Create user props

---
+++ app/user/[username]/page.tsx
@@ -0,0 +1,16 @@
+import { createClient } from '@/utils/supabase/server'
+import UserPage from '@/components/UserPage/UserPage'
+import TeacherPage from '@/components/TeacherPage/TeacherPage'
+
+export default async function page({ params }: { params: { username: string } }) {
+	const { username } = params
+	const supabase = await createClient()
+
+	const { data: userData, error } = await supabase.from('profiles').select('*').eq('username', username).single()
+
+	if (error || !userData) {
+		return <p className='text-center text-red-500'>User not found</p>
+	}
+
+	return userData.isTeacher ? <TeacherPage userData={userData} /> : <UserPage userData={userData} />
+}
