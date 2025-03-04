baa6e7e54c245b214779f3d9fba86d3722e9ffea
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-04 20:34:14 +0100

    Use useDebouncedSeach and add Loader

--- components/ui/Navbar/NavbarSearch/NavbarSearch.tsx
+++ components/ui/Navbar/NavbarSearch/NavbarSearch.tsx
@@ -1,32 +1,17 @@
 'use client'
 
 import { Input } from '../../input'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { CourseProps } from '@/types/api'
 import SingleSearchCourse from '@/components/SingleSearchCourse/SingleSearchCourse'
+import useDebouncedSearch from '@/lib/useDebouncedSearch'
+import Loader from '../../loader'
 
 const NavbarSearch = () => {
 	const [inputValue, setInputValue] = useState('')
-	const [courses, setCourses] = useState([])
 	const [inputFocused, setInputFocused] = useState(false)
-
-	useEffect(() => {
-		const fetchCourses = async () => {
-			if (inputValue.trim().length < 2) {
-				setCourses([])
-				return
-			}
-
-			const res = await fetch(`/api/course/search?q=${inputValue}`)
-			const data = await res.json()
-			setCourses(data.courses)
-		}
-
-		const debounce = setTimeout(fetchCourses, 300)
-
-		return () => clearTimeout(debounce)
-	}, [inputValue])
+	const { courses, loading } = useDebouncedSearch(inputValue)
 
 	return (
 		<div className='flex-1 relative'>
@@ -40,10 +25,14 @@ const NavbarSearch = () => {
 				placeholder='Search for any courses...'
 				className='w-full rounded-xl'
 			/>
-			{inputValue.trim().length >= 3 && courses.length > 0 && inputFocused && (
-				<div className='w-full absolute top-full left-0 bg-secondary border border-border shadow-lg p-2 mt-2 rounded-md z-30'>
-					<ul>
-						{courses.map((course: CourseProps) => (
+			{inputValue.trim().length >= 3 && courses && courses.length > 0 && inputFocused && (
+				<div className='w-full absolute top-full left-0 bg-card border border-border shadow-lg p-2 mt-2 rounded-md z-30'>
+					{loading ? (
+						<div className='flex justify-center items-center py-8'>
+							<Loader />
+						</div>
+					) : (
+						courses?.map((course: CourseProps) => (
 							<Link
 								key={course.id}
 								href={`/course/${course.slug}`}
@@ -52,8 +41,8 @@ const NavbarSearch = () => {
 								}}>
 								<SingleSearchCourse course={course} />
 							</Link>
-						))}
-					</ul>
+						))
+					)}
 				</div>
 			)}
 		</div>
