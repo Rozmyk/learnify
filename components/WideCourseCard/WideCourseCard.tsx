ba1fae3a9b182b06fe2c0f8a7556cb0405654b0a
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-05 17:43:47 +0100

    and min width and height

--- components/WideCourseCard/WideCourseCard.tsx
+++ components/WideCourseCard/WideCourseCard.tsx
@@ -6,7 +6,7 @@ const WideCourseCard = ({ thumbnail, title, description, reviews, slug, level }:
 	return (
 		<Link href={`/course/${slug}`}>
 			<div className='w-full flex justify-start items-start gap-4 border-b border-border p-4 shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px]'>
-				<div className='h-36 w-64 relative rounded-lg overflow-hidden'>
+				<div className='h-36 w-64 min-h-36 min-w-64 relative rounded-lg overflow-hidden'>
 					<Image className='object-cover' alt='course photo' fill src={thumbnail} />
 				</div>
 				<div className='flex flex-col'>
