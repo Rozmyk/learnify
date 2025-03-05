bb28ff87ff1d90ad203c2995bcc9f7d7276cce4b
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-05 17:54:39 +0100

    Fetch courses by category

--- components/SingleCategoryPage/CoursesFilterableList/CoursesFilterableList.tsx
+++ components/SingleCategoryPage/CoursesFilterableList/CoursesFilterableList.tsx
@@ -1,20 +1,32 @@
 'use client'
+
 import { Button } from '@/components/ui/button'
 import { useState, useEffect } from 'react'
 import CoursesSelect from './CoursesSelect/CoursesSelect'
 import FilterAccordion from './FilterAccordion/FilterAccordion'
 import Loader from '@/components/ui/loader'
-import { CourseProps, FilterProps } from '@/types/api'
+import { CategoryProps, CourseProps } from '@/types/api'
 import { useSearchParams, useRouter } from 'next/navigation'
 import WideCourseCard from '@/components/WideCourseCard/WideCourseCard'
 
-const CoursesFilterableList = () => {
+const CoursesFilterableList = ({ category }: { category: CategoryProps }) => {
 	const [fullWidth, setFullWidth] = useState(true)
 	const [loading, setLoading] = useState(true)
-	const [filteredCourses, setFilteredCourses] = useState<CourseProps[] | null>(null)
+	const [filteredCourses, setFilteredCourses] = useState<CourseProps[]>([])
 	const [filters, setFilters] = useState<Record<string, string>>({})
 	const router = useRouter()
 	const searchParams = useSearchParams()
+
+	const fetchFilteredCourses = async (params: Record<string, string>) => {
+		setLoading(true)
+		params.category = category.id
+		const paramsToSend = new URLSearchParams(params).toString()
+		const response = await fetch(`/api/courses?${paramsToSend}`)
+		const data = await response.json()
+		console.log(data)
+		setFilteredCourses(data)
+		setLoading(false)
+	}
 
 	const handleFilter = (paramsToUpdate: Record<string, string>) => {
 		const params = new URLSearchParams(searchParams.toString())
@@ -24,19 +36,26 @@ const CoursesFilterableList = () => {
 		})
 
 		router.push(`?${params.toString()}`, { scroll: false })
+		setLoading(true)
 	}
 
 	const handleCleanFilter = () => {
 		const params = new URLSearchParams()
 		router.push(`?${params.toString()}`, { scroll: false })
 	}
+
 	useEffect(() => {
 		const params: Record<string, string> = {}
 		searchParams.forEach((value, key) => {
 			params[key] = value
 		})
-		console.log(params)
 		setFilters(params)
+
+		if (Object.keys(params).length === 0) {
+			fetchFilteredCourses({})
+		} else {
+			fetchFilteredCourses(params)
+		}
 	}, [searchParams])
 
 	return (
@@ -63,13 +82,21 @@ const CoursesFilterableList = () => {
 					}`}>
 					<FilterAccordion filters={filters} handleFilter={handleFilter} />
 				</div>
-				<div className={`transition-all  duration-500 ${!fullWidth ? 'w-full' : 'md:w-3/4'}`}>
+				<div className={`transition-all duration-500 ${!fullWidth ? 'w-full' : 'md:w-3/4'}`}>
 					{loading ? (
-						<div className='flex justify-center items-center p-8'>
+						<div className='flex justify-center w-full items-center p-8'>
 							<Loader />
 						</div>
 					) : (
-						<div className='flex flex-col gap-4'>items</div>
+						<div className='flex flex-col gap-4'>
+							{filteredCourses.length > 0 ? (
+								filteredCourses.map(course => <WideCourseCard key={course.id} {...course} />)
+							) : (
+								<div className='flex justify-center items-center'>
+									<p>No courses found</p>
+								</div>
+							)}
+						</div>
 					)}
 				</div>
 			</div>
