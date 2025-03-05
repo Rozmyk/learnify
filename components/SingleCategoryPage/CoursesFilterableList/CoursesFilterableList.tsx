7dae4303dfe66622fb2902181b90894137373888
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-05 13:30:53 +0100

    Add filter login (handleFilter, cleanFilter)

--- components/SingleCategoryPage/CoursesFilterableList/CoursesFilterableList.tsx
+++ components/SingleCategoryPage/CoursesFilterableList/CoursesFilterableList.tsx
@@ -1,17 +1,43 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import CoursesSelect from './CoursesSelect/CoursesSelect'
 import FilterAccordion from './FilterAccordion/FilterAccordion'
 import Loader from '@/components/ui/loader'
+import { CourseProps, FilterProps } from '@/types/api'
+import { useSearchParams, useRouter } from 'next/navigation'
 import WideCourseCard from '@/components/WideCourseCard/WideCourseCard'
 
 const CoursesFilterableList = () => {
 	const [fullWidth, setFullWidth] = useState(true)
-	const [loading, setLoading] = useState(false)
-	const [sortBy, setSortBy] = useState(null)
-	const [free, setFree] = useState(false)
-	const [payable, setPayable] = useState(false)
+	const [loading, setLoading] = useState(true)
+	const [filteredCourses, setFilteredCourses] = useState<CourseProps[] | null>(null)
+	const [filters, setFilters] = useState<Record<string, string>>({})
+	const router = useRouter()
+	const searchParams = useSearchParams()
+
+	const handleFilter = (paramsToUpdate: Record<string, string>) => {
+		const params = new URLSearchParams(searchParams.toString())
+
+		Object.entries(paramsToUpdate).forEach(([key, value]) => {
+			params.set(key, value)
+		})
+
+		router.push(`?${params.toString()}`, { scroll: false })
+	}
+
+	const handleCleanFilter = () => {
+		const params = new URLSearchParams()
+		router.push(`?${params.toString()}`, { scroll: false })
+	}
+	useEffect(() => {
+		const params: Record<string, string> = {}
+		searchParams.forEach((value, key) => {
+			params[key] = value
+		})
+		console.log(params)
+		setFilters(params)
+	}, [searchParams])
 
 	return (
 		<>
@@ -21,19 +47,21 @@ const CoursesFilterableList = () => {
 						onClick={() => {
 							setFullWidth(!fullWidth)
 						}}>
-						FIltruj
+						Filters
 					</Button>
-					<CoursesSelect />
-					<Button variant='ghost'>Clean filters</Button>
+					<CoursesSelect filters={filters} handleFilter={handleFilter} />
+					<Button onClick={handleCleanFilter} variant='ghost'>
+						Clean filters
+					</Button>
 				</div>
-				<p className='font-semibold'>1000 results</p>
+				<p className='font-semibold'>{filteredCourses?.length ?? 0} results</p>
 			</div>
-			<div className='flex justify-between items-start gap-8 mb-96'>
+			<div className='flex md:flex-row flex-col justify-between items-start gap-8 mb-96'>
 				<div
 					className={`overflow-hidden transition-all duration-500 ${
 						fullWidth ? 'w-full md:w-1/4 opacity-100' : 'max-w-0 opacity-0'
 					}`}>
-					<FilterAccordion />
+					<FilterAccordion filters={filters} handleFilter={handleFilter} />
 				</div>
 				<div className={`transition-all  duration-500 ${!fullWidth ? 'w-full' : 'md:w-3/4'}`}>
 					{loading ? (
