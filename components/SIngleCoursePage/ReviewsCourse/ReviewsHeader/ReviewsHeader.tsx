ec964d2453b00de0e0caf6fd42a36867e55abf96
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-06 12:26:13 +0100

    Change text size

--- components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
+++ components/SIngleCoursePage/ReviewsCourse/ReviewsHeader/ReviewsHeader.tsx
@@ -2,9 +2,9 @@ const ReviewsHeader = ({ avgRating, reviewCount }: { avgRating: number; reviewCo
 	return (
 		<div className='flex justify-start items-center gap-2  py-2 mb-2'>
 			<span className='text-orange-400 text-3xl'>★</span>
-			<p className='font-semibold text-2xl'>{avgRating?.toFixed(1)} course rating</p>
-			<span className='text-2xl text-muted-foreground'>&#8226;</span>
-			<p className='font-semibold text-2xl'>{reviewCount} ratings</p>
+			<p className='font-semibold  text-lg md:text-2xl'>{avgRating?.toFixed(1)} course rating</p>
+			<span className='text-lg md:text-2xltext-muted-foreground'>&#8226;</span>
+			<p className='font-semibold text-lg md:text-2xl'>{reviewCount} ratings</p>
 		</div>
 	)
 }
