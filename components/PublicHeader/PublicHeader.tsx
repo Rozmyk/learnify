d21c9843c7a124f1d0698afff0e307a6bba9152d
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-07 14:48:31 +0100

    Change styles for mobile device

--- components/PublicHeader/PublicHeader.tsx
+++ components/PublicHeader/PublicHeader.tsx
@@ -1,9 +1,10 @@
 const PublicHeader = () => {
 	return (
-		<header
-			className='relative w-full h-[400px] bg-blue-300 bg-center'
-			style={{ backgroundImage: "url('/headerImage.png')" }}>
-			<div className='absolute top-10 left-10 max-w-96 p-4 bg-card shadow-lg rounded-md'>
+		<header className='relative'>
+			<div
+				className=' w-full h-[400px] bg-blue-300 bg-center'
+				style={{ backgroundImage: "url('/headerImage.png')" }}></div>
+			<div className='md:absolute md:top-10 md:left-10 max-w-96 p-4 bg-card shadow-lg rounded-md'>
 				<p className='text-3xl font-semibold'>Find what works for you</p>
 				<p className='text-muted-foreground'>
 					Courses from as low as 39.99 zł - promotion ends today. Learn topics that interest you, taught by experts with
