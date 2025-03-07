327130dcc0f52a739681c834813de29822fcde1b
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-07 14:08:29 +0100

    Add ChooseCourses to main no auth page

--- components/PublicHomePage/PublicHomePage.tsx
+++ components/PublicHomePage/PublicHomePage.tsx
@@ -1,6 +1,17 @@
 import PublicHeader from '../PublicHeader/PublicHeader'
+import SectionTitle from '../SectionTitle/SectionTitle'
+import ChooseCourse from './ChooseCourse/ChooseCourse'
 const PublicHomePage = () => {
-	return <PublicHeader />
+	return (
+		<>
+			<PublicHeader />
+
+			<SectionTitle additionalText='From key skills to technical issues, Udemy supports your professional development.'>
+				All the skills you need in one place
+			</SectionTitle>
+			<ChooseCourse />
+		</>
+	)
 }
 
 export default PublicHomePage
