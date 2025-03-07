b695a952e76833561ca9f4b56a80cd22d858007d
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-07 12:01:54 +0100

    Change location and split CourseCard into courseCard and CoursePopover

---
+++ components/ui/CourseCard/CourseCard.tsx
@@ -0,0 +1,92 @@
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState } from 'react'
+import { Popover, PopoverContent, PopoverTrigger, Portal } from '@radix-ui/react-popover'
+import { CourseProps } from '@/types/api'
+import StarRating from '../starRating'
+import { useCartStore } from '@/context/cart'
+import { useOwnedCoursesStore } from '@/context/ownedCourses'
+import CoursePopover from './CoursePopover/CoursePopover'
+const CourseCard = ({
+	thumbnail,
+	title,
+	price,
+	slug,
+	description,
+	profiles,
+	discount,
+	reviews,
+	id,
+	created_at,
+	skills_gained,
+	level,
+}: CourseProps) => {
+	const [open, setOpen] = useState(false)
+
+	const { addToCart, cartItems } = useCartStore()
+	const { owned } = useOwnedCoursesStore()
+
+	const isAlreadOwned = owned.some(item => item.course_id == id)
+	const isAlreadInCart = cartItems.some(item => item.product_id == id)
+
+	const updateLastViewedCourse = async () => {
+		try {
+			await fetch('/api/updateLastViewed', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ courseId: id }),
+			})
+		} catch (error) {
+			console.error('Error updating last viewed course:', error)
+		}
+	}
+
+	return (
+		<Popover open={open} onOpenChange={setOpen}>
+			<PopoverTrigger asChild>
+				<Link
+					onClick={updateLastViewedCourse}
+					href={`/course/${slug}`}
+					className='group no-underline flex cursor-pointer'
+					onMouseEnter={() => setOpen(true)}
+					onMouseLeave={() => setOpen(false)}>
+					<div className='max-w-60 w-60 '>
+						<div className='relative h-32 mb-1 border border-border'>
+							<Image fill src={thumbnail} alt='course photo' />
+						</div>
+						<h3 className='font-semibold line-clamp-2 '>{title}</h3>
+						<p className='capitalize text-sm text-muted-foreground'>{profiles.username}</p>
+						<StarRating reviews={reviews} />
+						<p className='font-semibold'>{price} zł</p>
+					</div>
+				</Link>
+			</PopoverTrigger>
+
+			<Portal>
+				<PopoverContent
+					className='w-96 bg-background rounded-lg shadow-lg p-4 border border-border z-[9999] animate-in fade-in-0 zoom-in-95'
+					side='right'
+					align='start'
+					sideOffset={8}
+					onMouseEnter={() => setOpen(true)}
+					onMouseLeave={() => setOpen(false)}>
+					<CoursePopover
+						slug={slug}
+						skills_gained={skills_gained}
+						created_at={created_at}
+						title={title}
+						description={description}
+						id={id}
+						level={level}
+						addToCart={addToCart}
+						isAlreadOwned={isAlreadOwned}
+						isAlreadInCart={isAlreadInCart}
+					/>
+				</PopoverContent>
+			</Portal>
+		</Popover>
+	)
+}
+
+export default CourseCard
---
+++ components/ui/CourseCard/CoursePopover/CoursePopover.tsx
@@ -0,0 +1,92 @@
+import { CircleAlert } from 'lucide-react'
+import Link from 'next/link'
+import { Button } from '../../button'
+import formatTimestamp from '@/lib/formatTimestamp'
+import SingleLessonPoint from '@/components/SingleLessonPoint/SingleLessonPoint'
+import FavButton from '@/components/FavButton/FavButton'
+const CoursePopover = ({
+	skills_gained,
+	created_at,
+	title,
+	description,
+	id,
+	level,
+	isAlreadOwned,
+	isAlreadInCart,
+	addToCart,
+    slug
+}: {
+	skills_gained: string
+	created_at: string
+	title: string
+	description: string
+	id: string
+	level: string
+	isAlreadOwned: boolean
+	isAlreadInCart: boolean
+    slug:string
+	addToCart: (id: string) => void
+}) => {
+	const splitStringToArray = (text: string) => {
+		if (!text) return []
+		return text
+			.split(';')
+			.map(sentence => sentence.trim())
+			.filter(sentence => sentence.length > 0)
+			.slice(0, 3)
+	}
+	const skillsArray = splitStringToArray(skills_gained)
+	return (
+		<>
+			{isAlreadOwned ? (
+				<div className='flex flex-col gap-4 '>
+					<div className='flex justify-start items-center gap-4'>
+						<CircleAlert />
+						<p className='text-muted-foreground '>
+							You have been attending this course since {formatTimestamp(created_at)}
+						</p>
+					</div>
+					<Link className='w-full' href={`/course/${slug}`}>
+						<Button className='w-full'>Go to course</Button>
+					</Link>
+				</div>
+			) : (
+				<>
+					<h3 className='text-lg font-semibold mb-2'>{title}</h3>
+					<div className='flex justify-start items-center gap-2'>
+						{' '}
+						<p className='text-green-400 text-xs'>Updated: {formatTimestamp(created_at)}</p>
+						<p className='text-xs text-muted-foreground'>Level: {level}</p>
+					</div>
+					<p className='text-sm  mt-2 mb-4'>{description}</p>
+					<div className='flex flex-col gap-2 mb-2'>
+						{skillsArray?.map(item => {
+							return <SingleLessonPoint key={item} value={item} />
+						})}
+					</div>
+					<div className='flex justify-between items-center gap-4'>
+						{isAlreadInCart ? (
+							<Link className='w-full' href='/cart'>
+								<Button className='w-full'>Go to cart</Button>
+							</Link>
+						) : (
+							<Button
+								className='w-full'
+								onClick={() => {
+									addToCart(id)
+								}}>
+								Add to cart
+							</Button>
+						)}
+
+						<div className='w-10'>
+							<FavButton courseId={id} />
+						</div>
+					</div>
+				</>
+			)}
+		</>
+	)
+}
+
+export default CoursePopover
