5b477bab48ddb1bbc1ed9a4c6a4b8642b436848e
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-07 18:09:47 +0100

    Fetch courses length to each categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -4,13 +4,28 @@ export async function GET() {
 	const supabase = await createClient()
 
 	try {
-		const { data, error } = await supabase.from('categories').select('*')
+		const { data: categories, error: categoriesError } = await supabase.from('categories').select('*')
 
-		if (error) {
-			return new Response(JSON.stringify({ error: error.message }), { status: 500 })
+		if (categoriesError) {
+			return new Response(JSON.stringify({ error: categoriesError.message }), { status: 500 })
 		}
 
-		return new Response(JSON.stringify(data), { status: 200 })
+		const categoriesWithCount = await Promise.all(
+			categories.map(async (category: { id: number }) => {
+				const { count, error: countError } = await supabase
+					.from('course')
+					.select('id', { count: 'exact' })
+					.eq('categories_id', category.id)
+
+				if (countError) {
+					return { ...category, courseCount: 0 }
+				}
+
+				return { ...category, courseCount: count }
+			})
+		)
+
+		return new Response(JSON.stringify(categoriesWithCount), { status: 200 })
 	} catch (error) {
 		return new Response(JSON.stringify({ error: 'Unexpected error occurred' }), { status: 500 })
 	}
