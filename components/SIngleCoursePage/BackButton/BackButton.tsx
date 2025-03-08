0a16ed9cd4156d9ec00d6c4cdbbbb31b14c7ae93
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-08 14:47:31 +0100

    Change text color

--- components/SIngleCoursePage/BackButton/BackButton.tsx
+++ components/SIngleCoursePage/BackButton/BackButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const BackButton = () => {
 	return (
 		<Link
-			className='absolute top-4 left-4 z-20 cursor-pointer font-semibold text-sm flex justify-start items-center gap-2'
+			className='absolute top-4 left-4 z-20 text-white cursor-pointer font-semibold text-sm flex justify-start items-center gap-2'
 			href='/'>
 			<ArrowLeft size={16} />
 			Back to courses
