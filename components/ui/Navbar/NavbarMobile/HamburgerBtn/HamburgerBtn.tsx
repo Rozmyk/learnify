71531233f577d8d0b4a9221a5a82f59f4f866228
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-08 14:18:16 +0100

    Change bars color

--- components/ui/Navbar/NavbarMobile/HamburgerBtn/HamburgerBtn.tsx
+++ components/ui/Navbar/NavbarMobile/HamburgerBtn/HamburgerBtn.tsx
@@ -2,15 +2,15 @@ const HamburgerBtn = ({ handleOpen, isOpen }: { handleOpen: () => void; isOpen: 
 	return (
 		<button onClick={handleOpen} className='flex flex-col justify-center items-center p-2'>
 			<span
-				className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+				className={`bg-primary block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
 					isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
 				}`}></span>
 			<span
-				className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+				className={`bg-primary block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
 					isOpen ? 'opacity-0' : 'opacity-100'
 				}`}></span>
 			<span
-				className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+				className={`bg-primary block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
 					isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
 				}`}></span>
 		</button>
