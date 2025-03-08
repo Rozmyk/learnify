9497aacdbdc83f14ebf8736f4334bb8cde61ea77
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-08 14:20:11 +0100

    Imporve mobile layout

--- components/WideCourseCard/WideCourseCard.tsx
+++ components/WideCourseCard/WideCourseCard.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 const WideCourseCard = ({ thumbnail, title, description, reviews, slug, level, price, discount }: CourseProps) => {
 	return (
 		<Link href={`/course/${slug}`}>
-			<div className='w-full flex justify-between items-start gap-8 border-b border-border p-4 shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px]'>
-				<div className='h-36 w-64 min-h-36 min-w-64 relative rounded-lg overflow-hidden'>
+			<div className='w-full flex flex-col md:flex-row justify-between items-start gap-8 border-b border-border p-4 shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:translate-y-[-4px]'>
+				<div className='h-36  w-full md:w-64 min-h-36 min-w-64 relative rounded-lg overflow-hidden'>
 					<Image className='object-cover' alt='course photo' fill src={thumbnail} />
 				</div>
 				<div className='flex flex-col flex-1'>
