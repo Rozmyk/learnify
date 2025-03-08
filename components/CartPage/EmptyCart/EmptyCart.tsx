0630d4f8912e4094942b40f00463a710121917aa
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-08 14:18:28 +0100

    Add text align center to title

--- components/CartPage/EmptyCart/EmptyCart.tsx
+++ components/CartPage/EmptyCart/EmptyCart.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link'
 const EmptyCart = () => {
 	return (
 		<div className='flex flex-col gap-4 justify-center items-center w-full h-96 border border-border rounded-lg '>
-			<p className='text-lg font-semibold'>Your shopping cart is empty. Continue shopping and find a course.</p>
+			<p className='text-lg font-semibold text-center'>
+				Your shopping cart is empty. Continue shopping and find a course.
+			</p>
 			<Link href={'/'}>
 				<Button>Continue shopping</Button>
 			</Link>
