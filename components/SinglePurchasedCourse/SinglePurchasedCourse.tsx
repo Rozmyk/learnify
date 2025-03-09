f06f51f4af47cd35d3144a9495b4e8cc545b861a
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-09 17:29:56 +0100

    Add hover action

--- components/SinglePurchasedCourse/SinglePurchasedCourse.tsx
+++ components/SinglePurchasedCourse/SinglePurchasedCourse.tsx
@@ -1,18 +1,38 @@
+'use client'
 import { CourseProps } from '@/types/api'
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import ProgressComponent from '../ProgressComponent/ProgressComponent'
 import SimpleStarRating from '../SimpleStarRating/SimpleStarRating'
+import { CirclePlay } from 'lucide-react'
 
 const SinglePurchasedCourse = ({ title, profiles, thumbnail, reviews, slug }: CourseProps) => {
+	const [isHovered, setIsHovered] = useState(false)
 	return (
 		<Link href={`/course/${slug}`}>
 			<div className='w-80 max-w-80 mix-w-80  p-2'>
-				<div className='relative w-full h-40'>
+				<div
+					onMouseEnter={() => {
+						setIsHovered(true)
+					}}
+					onMouseLeave={() => {
+						setIsHovered(false)
+					}}
+					className='relative w-full h-40 border border-border'>
+					{isHovered && (
+						<div className='absolute opacity-70 bg-black top-0 left-0 right-0 bottom-0 w-full h-full z-50 flex justify-center items-center'>
+							<div className='opacity-100'>
+								{' '}
+								<CirclePlay size={50} />
+							</div>
+						</div>
+					)}
 					<Image fill src={thumbnail} alt='course image' />
 				</div>
-				<h3 className='line-clamp-2 font-semibold mt-2'>{title}</h3>
-				<p className='capitalize text-muted-foreground text-sm mb-4'>{profiles.username}</p>
+				<h3 className='line-clamp-2 font-semibold mt-2 h-[3rem] leading-snug'>{title}</h3>
+
+				<p className='capitalize text-muted-foreground text-sm mb-6'>{profiles.username}</p>
 				<ProgressComponent value={95} />
 				<div className='flex justify-between items-center py-2'>
 					<p className='text-xs text-muted-foreground'>Completed 95%</p>
