6b9daf498cb3a6f6bd4217505dbab654933258f0
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-09 12:21:39 +0100

    Create ProgressComponent.tsx

---
+++ components/ProgressComponent/ProgressComponent.tsx
@@ -0,0 +1,11 @@
+import * as Progress from '@radix-ui/react-progress'
+
+const ProgressComponent = ({ value }: { value: number }) => {
+	return (
+		<Progress.Root className='relative overflow-hidden bg-primary rounded-full w-full h-0.5'>
+			<Progress.Indicator className='bg-purple-400 h-full transition-all duration-300' style={{ width: `${value}%` }} />
+		</Progress.Root>
+	)
+}
+
+export default ProgressComponent
