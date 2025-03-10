127e8b8210f3bb54839af4bcc6120d86222fb180
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 13:20:24 +0100

    Update styles and g uard when favourite is null

--- components/MyWishlistPage/MyWishlistPage.tsx
+++ components/MyWishlistPage/MyWishlistPage.tsx
@@ -2,18 +2,28 @@
 import { useWishlistStore } from '@/context/wishlist'
 import Loader from '../ui/loader'
 import SingleWishlistCard from './SingleWishlistCard/SingleWishlistCard'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 const MyWishlistPage = () => {
 	const { favorites, loading } = useWishlistStore()
 	return loading ? (
-		<div>
+		<div className='flex justify-center items-center w-full py-8'>
 			<Loader />
 		</div>
 	) : (
-		<div className='flex '>
-			{favorites.map(item => {
-				return <SingleWishlistCard {...item} key={item.id} />
-			})}
+		<div className='flex min-h-96 '>
+			{favorites.length > 0 ? (
+				favorites.map(item => {
+					return <SingleWishlistCard {...item} key={item.id} />
+				})
+			) : (
+				<div className='flex justify-center items-center w-full p-8'>
+					<Link href='/'>
+						<Button>Browse Courses</Button>
+					</Link>
+				</div>
+			)}
 		</div>
 	)
 }
