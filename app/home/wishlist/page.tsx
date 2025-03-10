2aa2d9b8f6745eee6de8808c44910c49e93b6109
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 13:23:10 +0100

    Add guard when user is no auth

--- app/home/wishlist/page.tsx
+++ app/home/wishlist/page.tsx
@@ -1,4 +1,13 @@
 import MyWishlistPage from '@/components/MyWishlistPage/MyWishlistPage'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 export default async function MyCourses() {
+	const supabase = await createClient()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+	if (!user) {
+		redirect('/')
+	}
 	return <MyWishlistPage />
 }
