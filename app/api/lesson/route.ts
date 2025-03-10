3e2a6b87797d9db317c38db139a35af33182f5bc
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 16:10:12 +0100

    Create endpoint to fetch course lessons

---
+++ app/api/lesson/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(req: Request) {
+	const supabase = await createClient()
+	const { searchParams } = new URL(req.url)
+	const course_id = searchParams.get('course_id')
+	const limit = searchParams.get('limit')
+
+	if (!course_id) {
+		return NextResponse.json({ courses: [] })
+	}
+
+	let query = supabase.from('lesson').select('*').eq('course_id', course_id)
+
+	if (limit) {
+		query = query.limit(Number(limit))
+	}
+
+	const { data, error } = await query
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
+
+	return NextResponse.json(data)
+}
