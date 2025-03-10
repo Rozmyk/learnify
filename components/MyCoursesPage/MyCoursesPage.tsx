ee99549cba13823a8865f640ae133c20aff00dfd
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 13:23:29 +0100

    Delete old props and add loader

--- components/MyCoursesPage/MyCoursesPage.tsx
+++ components/MyCoursesPage/MyCoursesPage.tsx
@@ -1,15 +1,18 @@
-import { CourseProps } from '@/types/api'
-import CourseCard from '../ui/CourseCard/CourseCard'
+'use client'
 import SinglePurchasedCourse from '../SinglePurchasedCourse/SinglePurchasedCourse'
-
-const MyCoursesPage = ({ courses }: { courses: CourseProps[] | null }) => {
-	console.log(courses)
-	return (
-		<div className='grid  grid-cols-1 md:grid-cols-4 gap-4'>
-			{courses &&
-				courses.length > 0 &&
-				courses.map(item => {
-					return <SinglePurchasedCourse {...item} key={item.id} />
+import { useOwnedCoursesStore } from '@/context/ownedCourses'
+import Loader from '../ui/loader'
+const MyCoursesPage = () => {
+	const { owned, loading } = useOwnedCoursesStore()
+	return loading ? (
+		<div className='flex justify-center items-center w-full h-full py-8'>
+			<Loader />
+		</div>
+	) : (
+		<div className='grid  grid-cols-1 md:grid-cols-4 gap-8 min-h-screen'>
+			{owned &&
+				owned.map(item => {
+					return <SinglePurchasedCourse {...item.course} key={item.id} />
 				})}
 		</div>
 	)
