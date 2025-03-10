1e4cdfa710d5fa402433fe3412f8557e5bc134d7
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 13:22:58 +0100

    Delete old fetch function and add guard when user is no auth

--- app/home/my-courses/page.tsx
+++ app/home/my-courses/page.tsx
@@ -9,21 +9,6 @@ export default async function MyCourses() {
 	if (!user) {
 		redirect('/')
 	}
-	const { data, error } = await supabase
-		.from('course')
-		.select(
-			`
-              *,
-              reviews(*, profiles(*)),
-              categories(*),
-              profiles(*)
-             
-            `
-		)
-		.eq('author_id', user.id)
-	if (error) {
-		console.log(error.message)
-	}
 
-	return <MyCoursesPage courses={data} />
+	return <MyCoursesPage />
 }
