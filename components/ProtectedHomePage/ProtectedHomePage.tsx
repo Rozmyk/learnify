5aed1735bcd7d994ff6abc74e587f65d3c1d472e
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 13:19:55 +0100

    Add BegginerCourses to componenet

--- components/ProtectedHomePage/ProtectedHomePage.tsx
+++ components/ProtectedHomePage/ProtectedHomePage.tsx
@@ -3,6 +3,7 @@ import { ProfileDataProps, ReviewProps } from '@/types/api'
 import RecommendedCourses from '../ProtectedHeader/RecommendedCourses/RecommendedCourses'
 import PromotedCourse from '../ProtectedHeader/PromotedCourse/PromotedCourse'
 import PersonalizedCourse from '../ProtectedHeader/PersonalizedCourse/PersonalizedCourse'
+import BegginerCourses from '../ProtectedHeader/BegginerCourses/BegginerCourses'
 import { createClient } from '@/utils/supabase/server'
 export default async function ProtectedHomePage({ profileData }: { profileData: ProfileDataProps }) {
 	const supabase = await createClient()
@@ -23,6 +24,7 @@ export default async function ProtectedHomePage({ profileData }: { profileData: 
 			{courses && <RecommendedCourses courses={courses} />}
 			<PromotedCourse />
 			<PersonalizedCourse lastViewedCourseId={profileData.lastViewedCourseId} />
+			<BegginerCourses />
 		</>
 	)
 }
