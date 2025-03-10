819a3ae2c003609f39158a93c35c98bd96ab82f4
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-10 12:17:03 +0100

    Add MyWishlistPage

--- app/home/wishlist/page.tsx
+++ app/home/wishlist/page.tsx
@@ -1,29 +1,4 @@
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
-import MyCoursesPage from '@/components/MyCoursesPage/MyCoursesPage'
+import MyWishlistPage from '@/components/MyWishlistPage/MyWishlistPage'
 export default async function MyCourses() {
-	const supabase = await createClient()
-	const {
-		data: { user },
-	} = await supabase.auth.getUser()
-	if (!user) {
-		redirect('/')
-	}
-	const { data, error } = await supabase
-		.from('course')
-		.select(
-			`
-              *,
-              reviews(*, profiles(*)),
-              categories(*),
-              profiles(*)
-             
-            `
-		)
-		.eq('author_id', user.id)
-	if (error) {
-		console.log(error.message)
-	}
-
-	return
+	return <MyWishlistPage />
 }
