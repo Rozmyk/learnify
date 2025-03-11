3e8b1577735dd68f6487475723d5a38b21e8ae0c
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-11 16:26:48 +0100

    Add content props

--- components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/VideoModal/VideoModal.tsx
+++ components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/VideoModal/VideoModal.tsx
@@ -1,14 +1,8 @@
 import ReactPlayer from 'react-player'
-const VideoModal = () => {
+const VideoModal = ({ content }: { content: string }) => {
 	return (
 		<div>
-			<ReactPlayer
-				url={'https://www.youtube.com/watch?v=enX8M-i8Joc'}
-				playing={true}
-				controls={true}
-				width='100%'
-				height='500px'
-			/>
+			<ReactPlayer url={content} playing={true} controls={true} width='100%' height='500px' />
 		</div>
 	)
 }
