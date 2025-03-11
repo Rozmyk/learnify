f3a75a4fd7234b2f183df8b5e5ec3f84bbbe124d
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-11 17:10:29 +0100

    Add EditorContent from tiptap

--- components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/NoteModal/NoteModal.tsx
+++ components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/NoteModal/NoteModal.tsx
@@ -1,7 +1,14 @@
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 const NoteModal = ({ content }: { content: string }) => {
+	const editor = useEditor({
+		extensions: [StarterKit],
+		editable: false,
+		content,
+	})
 	return (
-		<div>
-			<p>{content}</p>
+		<div className='prose max-w-none'>
+			<EditorContent editor={editor} />
 		</div>
 	)
 }
