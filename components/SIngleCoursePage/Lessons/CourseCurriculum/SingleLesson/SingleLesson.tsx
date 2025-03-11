ffb94b55d1ede693dc6e141d41395390255d60aa
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-11 16:10:28 +0100

    Add clickOutside Action and add NoteModal and VideoModal components

--- components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/SingleLesson.tsx
+++ components/SIngleCoursePage/Lessons/CourseCurriculum/SingleLesson/SingleLesson.tsx
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import { SingleLessonProps } from '@/types/api'
 import { Button } from '@/components/ui/button'
 import * as Portal from '@radix-ui/react-portal'
-import ReactPlayer from 'react-player'
+import NoteModal from './NoteModal/NoteModal'
+import VideoModal from './VideoModal/VideoModal'
 const SingleLesson = ({ lesson }: { lesson: SingleLessonProps }) => {
 	const [showModal, setShowModal] = useState(false)
 	return (
@@ -28,31 +29,22 @@ const SingleLesson = ({ lesson }: { lesson: SingleLessonProps }) => {
 			</div>
 			{showModal && (
 				<Portal.Root>
-					<div className='fixed inset-0 z-50 bg-black/80 flex items-center justify-center'>
-						<div className='bg-background w-full max-w-2xl  rounded-lg shadow-lg p-6 relative'>
-							<div className='flex items-center justify-between w-full mb-2 '>
+					<div
+						onClick={() => setShowModal(false)}
+						className='fixed inset-0 z-50 bg-black/80 flex items-center justify-center'>
+						<div
+							onClick={e => e.stopPropagation()}
+							className='bg-background w-full max-w-2xl rounded-lg shadow-lg p-6 relative'>
+							<div className='flex items-center justify-between w-full mb-2'>
 								<div className='flex flex-col justify-start items-start'>
 									<p className='text-sm text-muted-foreground'>Course preview</p>
 									<p className='text-lg font-semibold'>{lesson.title}</p>
 								</div>
-								<Button
-									size='icon'
-									variant='ghost'
-									onClick={() => {
-										setShowModal(false)
-									}}>
+								<Button size='icon' variant='ghost' onClick={() => setShowModal(false)}>
 									<X />
 								</Button>
 							</div>
-							<div>
-								<ReactPlayer
-									url={'https://www.youtube.com/watch?v=enX8M-i8Joc'}
-									playing={true}
-									controls={true}
-									width='100%'
-									height='500px'
-								/>
-							</div>
+							{lesson.is_video ? <VideoModal /> : <NoteModal />}
 						</div>
 					</div>
 				</Portal.Root>
