00c964ac6fa305864578765483e6de6dd340cb80
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-12 18:09:18 +0100

    Add more component

--- app/course/[id]/learn/[lessonId]/layout.tsx
+++ app/course/[id]/learn/[lessonId]/layout.tsx
@@ -1,17 +1,22 @@
 import LessonsSidebar from '@/components/SingleLessonPage/LessonsSiderbar/LessonsSidebar'
+import CourseDetails from '@/components/SingleLessonPage/CourseDetails/CourseDetails'
 export default async function Layout({
 	children,
 	params,
 }: {
 	children: React.ReactNode
-	params: Promise<{ lessonId: string }>
+	params: Promise<{ lessonId: string; id: string }>
 }) {
 	const lessonId = (await params).lessonId
+	const courseId = (await params).id
 	return (
-		<div className='p-4 flex justify-between items-start'>
-			<div className='w-full h-full '>{children}</div>
-			<div className=' w-96 h-full'>
-				<LessonsSidebar lessonId={lessonId} />
+		<div className=' flex justify-between items-start'>
+			<div className='w-full h-full  '>
+				{children}
+				<CourseDetails courseId={courseId} />
+			</div>
+			<div className=' max-w-[420px] w-full h-screen bg-background relative '>
+				<LessonsSidebar lessonId={lessonId} courseId={courseId} />
 			</div>
 		</div>
 	)
