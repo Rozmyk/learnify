f7d923738cd432ec75c6c05cf4d826cf474a839d
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-12 14:43:02 +0100

    Use SIngleLessonPage

--- app/course/[id]/learn/[lessonId]/page.tsx
+++ app/course/[id]/learn/[lessonId]/page.tsx
@@ -1,5 +1,16 @@
+import userHasAccessToLesson from '@/lib/userHasAccesToLesson'
+import { createClient } from '@/utils/supabase/server'
+import SingleLessonPage from '@/components/SingleLessonPage/SingleLessonPage'
 export default async function Page({ params }: { params: Promise<{ lessonId: string }> }) {
+	const supabase = await createClient()
 	const lessonId = (await params).lessonId
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+	if (user) {
+		const haveToAcces = await userHasAccessToLesson(lessonId, user.id)
+		console.log(haveToAcces)
+	}
 
-	return <div>lesson id {lessonId}</div>
+	return <SingleLessonPage lessonId={lessonId} />
 }
