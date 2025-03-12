8edb08b46903e62ae80bc1fce35cc75e78704f60
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-12 18:08:37 +0100

    Create CourseDetails.tsx

---
+++ components/SingleLessonPage/CourseDetails/CourseDetails.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { CourseProps } from '@/types/api'
+import { useState, useEffect } from 'react'
+import Loader from '@/components/ui/loader'
+const CourseDetails = ({ courseId }: { courseId: string }) => {
+	const [courseData, setCourseData] = useState<CourseProps | null>(null)
+	const [loading, setLoading] = useState(true)
+	useEffect(() => {
+		const fetchCourseData = async () => {
+			try {
+				const response = await fetch(`/api/course/id?id=${courseId}`)
+
+				if (!response.ok) {
+					throw new Error('Failed to fetch course')
+				}
+				const data = await response.json()
+				setCourseData(data)
+				setLoading(false)
+			} catch (err) {
+				console.log(err)
+			}
+		}
+		if (courseId) {
+			fetchCourseData()
+		}
+	}, [courseId])
+
+	if (loading) {
+		return (
+			<div className='w-full  flex justify-center items-center py-8 h-screen'>
+				<Loader />
+			</div>
+		)
+	}
+	return (
+		<div className='h-screen'>
+			course details :{courseId}
+			{courseData.title}
+		</div>
+	)
+}
+
+export default CourseDetails
