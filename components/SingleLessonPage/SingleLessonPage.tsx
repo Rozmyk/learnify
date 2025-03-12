f966dd6640decc21bfe11f8360569e7e11b8ab09
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-12 18:09:07 +0100

    Fetch data and add reactplayer

--- components/SingleLessonPage/SingleLessonPage.tsx
+++ components/SingleLessonPage/SingleLessonPage.tsx
@@ -1,11 +1,43 @@
 'use client'
+import { SingleLessonProps } from '@/types/api'
 import Loader from '../ui/loader'
-
+import { useState, useEffect } from 'react'
+import ReactPlayer from 'react-player'
 const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
+	const [lessonData, setLessonData] = useState<SingleLessonProps | null>(null)
+	const [loading, setLoading] = useState(true)
+	useEffect(() => {
+		if (!lessonId) return
+		const fetchLesson = async () => {
+			try {
+				const response = await fetch(`/api/get-single-lesson?lessonId=${lessonId}`)
+				if (!response.ok) {
+					throw new Error('Failed to fetch lesson')
+				}
+				const data = await response.json()
+				setLessonData(data.lesson)
+				setLoading(false)
+			} catch (err) {
+				console.log(err)
+			}
+		}
+		if (lessonId) {
+			fetchLesson()
+		}
+	}, [lessonId])
+	if (loading) {
+		return (
+			<div className='w-full h-full flex justify-center items-center py-8'>
+				<Loader />
+			</div>
+		)
+	}
 	return (
-		<div className='w-full h-full flex justify-center items-center py-8'>
-			<Loader />
-		</div>
+		lessonData && (
+			<div className='flex justify-center items-centerw w-full h-full'>
+				<ReactPlayer url={lessonData.video_url} playing={true} controls={true} width='100%' height='500px' />
+			</div>
+		)
 	)
 }
 
