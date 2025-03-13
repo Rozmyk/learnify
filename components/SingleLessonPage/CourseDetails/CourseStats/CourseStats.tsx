f37fe5c95c7bb1fb03d372452dd8fedcc09116d6
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-13 14:39:54 +0100

    Create CourseStats.tsx

---
+++ components/SingleLessonPage/CourseDetails/CourseStats/CourseStats.tsx
@@ -0,0 +1,41 @@
+import { addRatingsToCourses } from '@/lib/calcRatings'
+import { CourseProps } from '@/types/api'
+import { ReactNode } from 'react'
+import StarRating from '@/components/ui/starRating'
+import formatTimestamp from '@/lib/formatTimestamp'
+import StatItem from '@/components/StatItem/StatItem'
+import { Globe, CircleAlert } from 'lucide-react'
+const SingleStat = ({ children, text }: { children: ReactNode; text: string }) => {
+	return (
+		<div className='flex flex-col justify-center items-start'>
+			{children}
+			<p className='text-xs text-muted-foreground'>{text}</p>
+		</div>
+	)
+}
+
+const CourseStats = ({ courseData }: { courseData: CourseProps }) => {
+	const ratingData = addRatingsToCourses(courseData.reviews)
+
+	return (
+		<div className='flex flex-col'>
+			<div className='flex justify-start items-center gap-6 mb-4'>
+				<SingleStat text={`${ratingData.reviewCount} reviews`}>
+					<StarRating reviews={courseData.reviews} compact />
+				</SingleStat>
+				<SingleStat text={`${ratingData.reviewCount} reviews`}>
+					<StarRating reviews={courseData.reviews} compact />
+				</SingleStat>
+				<SingleStat text={`${ratingData.reviewCount} reviews`}>
+					<StarRating reviews={courseData.reviews} compact />
+				</SingleStat>
+			</div>
+			<div className='flex flex-col gap-2'>
+				<StatItem value={`Last update: ${formatTimestamp(courseData.created_at)}`} icon={CircleAlert} />
+				<StatItem value={`Language: ${courseData.language}`} icon={Globe} />
+			</div>
+		</div>
+	)
+}
+
+export default CourseStats
