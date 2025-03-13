132a1796d62a5b577e2c2d65a3fb9bf17086a8ba
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-13 18:17:31 +0100

    Add user Section

--- components/SingleLessonPage/CourseDetails/CourseDetails.tsx
+++ components/SingleLessonPage/CourseDetails/CourseDetails.tsx
@@ -2,8 +2,9 @@
 import { CourseProps } from '@/types/api'
 import { useState, useEffect } from 'react'
 import Loader from '@/components/ui/loader'
-import formatTimestamp from '@/lib/formatTimestamp'
 import CourseStats from './CourseStats/CourseStats'
+import DetailsBox from './DetailsBox/DetailsBox'
+import Image from 'next/image'
 const CourseDetails = ({ courseSlug }: { courseSlug: string }) => {
 	const [courseData, setCourseData] = useState<CourseProps | null>(null)
 	const [loading, setLoading] = useState(true)
@@ -15,6 +16,7 @@ const CourseDetails = ({ courseSlug }: { courseSlug: string }) => {
 				if (!response.ok) {
 					throw new Error('Failed to fetch course')
 				}
+
 				const data = await response.json()
 				setCourseData(data)
 				setLoading(false)
@@ -22,11 +24,11 @@ const CourseDetails = ({ courseSlug }: { courseSlug: string }) => {
 				console.log(err)
 			}
 		}
+
 		if (courseSlug) {
 			fetchCourseData()
 		}
 	}, [courseSlug])
-
 	if (loading) {
 		return (
 			<div className='w-full  flex justify-center items-center py-8 '>
@@ -38,6 +40,27 @@ const CourseDetails = ({ courseSlug }: { courseSlug: string }) => {
 		<div className='p-4'>
 			<h1 className='text-2xl font-semibold mb-4'>{courseData?.title}</h1>
 			{courseData && <CourseStats courseData={courseData} />}
+			<DetailsBox title='Details'>
+				<p>Skill level: {courseData?.level}</p>
+				<p>Language: {courseData?.language}</p> <p>Subtitles: No</p>
+			</DetailsBox>
+			<DetailsBox title='Description'>
+				<p>{courseData?.detailed_description}</p>
+			</DetailsBox>
+			{courseData && (
+				<DetailsBox title='Instructors'>
+					<div className='flex justify-start items-start gap-4'>
+						<div className='w-20 h-20 min-h-20 min-w-20 rounded-full overflow-hidden relative'>
+							<Image src={courseData?.profiles.avatar_url} fill alt='user photo' />
+						</div>
+						<div className='flex flex-col justify-start items-start'>
+							<h4 className='font-semibold capitalize text-primary'>{courseData.profiles.username}</h4>
+							<p>{courseData.profiles.header}</p>
+						</div>
+					</div>
+					<p className='mt-4'>{courseData.profiles.description}</p>
+				</DetailsBox>
+			)}
 		</div>
 	)
 }
