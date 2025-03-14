b144554272896835d2ebdefb46e23a54591c3d80
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-14 18:38:36 +0100

    Set Tiptap editor content after fetching lesson data

--- components/SingleLessonPage/SingleLessonPage.tsx
+++ components/SingleLessonPage/SingleLessonPage.tsx
@@ -1,19 +1,29 @@
 'use client'
+
 import { SingleLessonProps, UserLessonsProgressProps } from '@/types/api'
 import { useState, useEffect } from 'react'
 import ReactPlayer from 'react-player'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Skeleton from '../ui/skeleton'
+
 const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
 	const [lessonData, setLessonData] = useState<SingleLessonProps | null>(null)
 	const [progressData, setProgressData] = useState<UserLessonsProgressProps | null>(null)
 	const [loading, setLoading] = useState(true)
+
 	const editor = useEditor({
 		extensions: [StarterKit],
 		editable: false,
-		content: lessonData?.content_json,
+		content: '',
 	})
+
+	useEffect(() => {
+		if (lessonData?.content_json && editor) {
+			editor.commands.setContent(lessonData.content_json)
+		}
+	}, [lessonData?.content_json, editor])
+
 	const handleChangeWatched = async () => {
 		if (!progressData?.watched) {
 			try {
@@ -25,7 +35,6 @@ const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
 					body: JSON.stringify({ lessonId }),
 				})
 				const data = await response.json()
-
 				if (!response.ok) {
 					console.log(data.error)
 				}
@@ -40,12 +49,8 @@ const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
 		const fetchLesson = async () => {
 			try {
 				const response = await fetch(`/api/get-single-lesson?lessonId=${lessonId}`)
-
-				if (!response.ok) {
-					throw new Error('Failed to fetch lesson')
-				}
+				if (!response.ok) throw new Error('Failed to fetch lesson')
 				const data = await response.json()
-
 				setLessonData(data.lesson)
 				setProgressData(data.userLessonProgress)
 				setLoading(false)
@@ -53,20 +58,20 @@ const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
 				console.log(err)
 			}
 		}
-		if (lessonId) {
-			fetchLesson()
-		}
+		fetchLesson()
 	}, [lessonId])
+
 	if (loading) {
 		return (
 			<div className='w-full h-full flex justify-center items-center py-8'>
-				<Skeleton className='w-full h-[70vh] ' />
+				<Skeleton className='w-full h-[70vh]' />
 			</div>
 		)
 	}
+
 	return (
 		lessonData && (
-			<div className='flex justify-center items-centerw w-full h-full'>
+			<div className='flex justify-center items-center w-full h-full'>
 				{lessonData.video_url ? (
 					<ReactPlayer
 						onEnded={handleChangeWatched}
@@ -77,7 +82,7 @@ const SingleLessonPage = ({ lessonId }: { lessonId: string | null }) => {
 						height='70vh'
 					/>
 				) : (
-					<div className='prose max-w-none w-full h-[70vh]'>
+					<div className='prose max-w-none w-full h-[70vh] overflow-y-auto px-4'>
 						<EditorContent editor={editor} />
 					</div>
 				)}
