6f48d605cdaf15cf1b75a5e78f89530653d19dbb
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-14 11:51:54 +0100

    Change id to slug

--- app/course/[slug]/page.tsx
+++ app/course/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import SingleCoursePage from '@/components/SIngleCoursePage/SingleCoursePage'
 import { createClient } from '@/utils/supabase/server'
 import PageContainer from '@/components/PageContainer/PageContainer'
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-	const id = (await params).id
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+	const slug = (await params).slug
 	const supabase = await createClient()
 	const { data, error } = await supabase
 		.from('course')
@@ -15,10 +15,11 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
 				 
 				`
 		)
-		.eq('slug', id)
+		.eq('slug', slug)
 		.single()
 
 	if (error) {
+		console.log(error)
 		console.log(error.message)
 	}
 	return (
