3804896cfaa40041785a957c2a55877c6b6ddb89
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-14 11:58:54 +0100

    Improve guard when user is null or dont have access

--- app/course/[slug]/learn/[lessonId]/page.tsx
+++ app/course/[slug]/learn/[lessonId]/page.tsx
@@ -1,14 +1,20 @@
 import userHasAccessToLesson from '@/lib/userHasAccesToLesson'
 import { createClient } from '@/utils/supabase/server'
 import SingleLessonPage from '@/components/SingleLessonPage/SingleLessonPage'
+import { redirect } from 'next/navigation'
 export default async function Page({ params }: { params: Promise<{ lessonId: string }> }) {
 	const supabase = await createClient()
 	const lessonId = (await params).lessonId
 	const {
 		data: { user },
 	} = await supabase.auth.getUser()
-	if (user) {
-		const haveToAcces = await userHasAccessToLesson(lessonId, user.id)
+	if (!user) {
+		redirect('/')
+	}
+
+	const haveToAcces = await userHasAccessToLesson(lessonId, user.id)
+	if (!haveToAcces) {
+		redirect('/')
 	}
 
 	return <SingleLessonPage lessonId={lessonId} />
