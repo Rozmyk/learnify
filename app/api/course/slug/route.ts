c84d3f619cdbe188716359c1d20275683b1f75b4
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-14 13:23:43 +0100

    Change endpoint direction

---
+++ app/api/course/slug/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(req: Request) {
+	const supabase = await createClient()
+	const { searchParams } = new URL(req.url)
+	const slug = searchParams.get('slug')
+
+	if (!slug) {
+		return NextResponse.json({ courses: [] })
+	}
+
+	let query = supabase.from('course').select('*, categories(*), profiles(*), reviews(*)').eq('slug', slug).single()
+
+	const { data, error } = await query
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
+
+	return NextResponse.json(data)
+}
