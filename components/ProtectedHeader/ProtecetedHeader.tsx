d9308d685c3a046dece1ea5178fa58fe0cde5076
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 17:48:00 +0100

    Change text size

--- components/ProtectedHeader/ProtecetedHeader.tsx
+++ components/ProtectedHeader/ProtecetedHeader.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link'
 import WelcomeMessage from './WelcomeMessage/WelcomeMessage'
 import { ProfileDataProps } from '@/types/api'
 const ProtectedHeader = ({ profileData }: { profileData: ProfileDataProps }) => {
 	return (
 		<div>
 			<WelcomeMessage username={profileData.username} userPhoto={profileData.avatar_url} />
-			{/* <div className='flex justify-between items-center w-full'>
-				<p>Start learning</p>
-				<Link href={'/courses'}>Your courses:</Link>
-			</div> */}
 		</div>
 	)
 }
