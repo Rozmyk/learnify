111ffa7b454d69cd9abe26ce6b36584de3797dee
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 17:41:02 +0100

    Create endpoint to fetch courses owner length

---
+++ app/api/calc-course-owner/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server'
+
+export async function GET(req: Request) {
+	const supabase = await createClient()
+	const { searchParams } = new URL(req.url)
+	const course_id = searchParams.get('course_id')
+
+	if (!course_id) {
+		return NextResponse.json({ courses: [] })
+	}
+
+	let query = supabase.from('owned_courses').select('id').eq('course_id', course_id)
+
+	const { data, error } = await query
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 })
+	}
+
+	return NextResponse.json(data.length)
+}
