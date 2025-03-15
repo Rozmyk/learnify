a736fad6f29e81facd58698b7328ac276f7dd8af
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 16:20:37 +0100

    Add StartLearning to component

--- components/ProtectedHomePage/ProtectedHomePage.tsx
+++ components/ProtectedHomePage/ProtectedHomePage.tsx
@@ -1,10 +1,11 @@
 import ProtectedHeader from '../ProtectedHeader/ProtecetedHeader'
-import { ProfileDataProps, ReviewProps } from '@/types/api'
+import { ProfileDataProps } from '@/types/api'
 import RecommendedCourses from '../ProtectedHeader/RecommendedCourses/RecommendedCourses'
 import PromotedCourse from '../ProtectedHeader/PromotedCourse/PromotedCourse'
 import PersonalizedCourse from '../ProtectedHeader/PersonalizedCourse/PersonalizedCourse'
 import BegginerCourses from '../ProtectedHeader/BegginerCourses/BegginerCourses'
 import { createClient } from '@/utils/supabase/server'
+import StartLearning from '../ProtectedHeader/StartLearning/StartLearning'
 export default async function ProtectedHomePage({ profileData }: { profileData: ProfileDataProps }) {
 	const supabase = await createClient()
 	const { data: courses, error: coursesError } = await supabase.from('course').select(`
@@ -21,6 +22,7 @@ export default async function ProtectedHomePage({ profileData }: { profileData: 
 	return (
 		<>
 			<ProtectedHeader profileData={profileData} />
+			<StartLearning />
 			{courses && <RecommendedCourses courses={courses} />}
 			<PromotedCourse />
 			<PersonalizedCourse lastViewedCourseId={profileData.lastViewedCourseId} />
