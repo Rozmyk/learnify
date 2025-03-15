8cb4c1f846c5e9305d3caf7490320d0c8f19a293
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 17:55:52 +0100

    Wrap div by Link and add redirect to course learn

--- components/ui/Navbar/MyCoursesButton/SingleOwnedCourse/SingleOwnedCourse.tsx
+++ components/ui/Navbar/MyCoursesButton/SingleOwnedCourse/SingleOwnedCourse.tsx
@@ -1,19 +1,22 @@
 import { CourseProps } from '@/types/api'
 import Image from 'next/image'
 import ProgressComponent from '@/components/ProgressComponent/ProgressComponent'
+import Link from 'next/link'
 
-const SingleOwnedCourse = ({ thumbnail, title, user_lessons_progress, lessons }: CourseProps) => {
+const SingleOwnedCourse = ({ thumbnail, title, user_lessons_progress, lessons, slug }: CourseProps) => {
 	const courseProgress = user_lessons_progress.length > 0 ? (user_lessons_progress.length / lessons.length) * 100 : 0
 	return (
-		<div className='flex gap-4 p-2 items-start justify-between'>
-			<div className='w-16 h-16 min-w-16 min-h-16 relative rounded-lg overflow-hidden'>
-				<Image src={thumbnail} className='object-cover' alt='course photo' fill />
+		<Link href={`/course/${slug}/learn`}>
+			<div className='flex gap-4 p-2 items-start justify-between'>
+				<div className='w-16 h-16 min-w-16 min-h-16 relative rounded-lg overflow-hidden'>
+					<Image src={thumbnail} className='object-cover' alt='course photo' fill />
+				</div>
+				<div className='flex-1'>
+					<p className='font-semibold text-sm mb-2 line-clamp-2  '>{title}</p>
+					<ProgressComponent value={courseProgress} />
+				</div>
 			</div>
-			<div className='flex-1'>
-				<p className='font-semibold text-sm mb-2'>{title}</p>
-				<ProgressComponent value={courseProgress} />
-			</div>
-		</div>
+		</Link>
 	)
 }
 
