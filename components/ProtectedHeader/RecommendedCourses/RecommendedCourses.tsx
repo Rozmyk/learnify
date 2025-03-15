b9e8b52c5aee0c49ee7868bacac915a045064876
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 16:20:45 +0100

    Change text size

--- components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
+++ components/ProtectedHeader/RecommendedCourses/RecommendedCourses.tsx
@@ -3,7 +3,7 @@ import CoursesCarousel from '@/components/CoursesCarousel/CoursesCarousel'
 const RecommendedCourses = ({ courses }: { courses: CourseProps[] }) => {
 	return (
 		<div>
-			<h2 className='text-4xl mb-5'>What will you learn now?</h2>
+			<h2 className='text-3xl font-semibold mb-5'>What will you learn now?</h2>
 
 			<CoursesCarousel text='Recommended for you' courses={courses} />
 		</div>
