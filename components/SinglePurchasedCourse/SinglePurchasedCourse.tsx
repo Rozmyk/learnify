ca0e6b21a66a35657b08a35d93ccda168d7269e6
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-15 16:19:55 +0100

    Update link href, change progress value, calc courseProgress

--- components/SinglePurchasedCourse/SinglePurchasedCourse.tsx
+++ components/SinglePurchasedCourse/SinglePurchasedCourse.tsx
@@ -7,10 +7,11 @@ import ProgressComponent from '../ProgressComponent/ProgressComponent'
 import SimpleStarRating from '../SimpleStarRating/SimpleStarRating'
 import { CirclePlay } from 'lucide-react'
 
-const SinglePurchasedCourse = ({ title, profiles, thumbnail, reviews, slug }: CourseProps) => {
+const SinglePurchasedCourse = ({ title, profiles, thumbnail, slug, user_lessons_progress, lessons }: CourseProps) => {
 	const [isHovered, setIsHovered] = useState(false)
+	const courseProgress = user_lessons_progress.length > 0 ? (user_lessons_progress.length / lessons.length) * 100 : 0
 	return (
-		<Link href={`/course/${slug}`}>
+		<Link href={`/course/${slug}/learn`}>
 			<div className='w-80 max-w-80 mix-w-80  p-2'>
 				<div
 					onMouseEnter={() => {
@@ -33,9 +34,9 @@ const SinglePurchasedCourse = ({ title, profiles, thumbnail, reviews, slug }: Co
 				<h3 className='line-clamp-2 font-semibold mt-2 h-[3rem] leading-snug'>{title}</h3>
 
 				<p className='capitalize text-muted-foreground text-sm mb-6'>{profiles.username}</p>
-				<ProgressComponent value={95} />
+				<ProgressComponent value={courseProgress} />
 				<div className='flex justify-between items-center py-2'>
-					<p className='text-xs text-muted-foreground'>Completed 95%</p>
+					<p className='text-xs text-muted-foreground'>Completed {courseProgress.toFixed(1)}%</p>
 					<div className='flex flex-col gap-.05'>
 						<SimpleStarRating stars={4} />
 						<p className='text-xs text-muted-foreground'>Your review</p>
