76ce6849179439cb9873b9dc7722083d581ad958
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-16 16:10:02 +0100

    Fetch courses from endpoint and add loader with logic

--- components/SIngleCoursePage/AlsoBought/AlsoBought.tsx
+++ components/SIngleCoursePage/AlsoBought/AlsoBought.tsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from 'react'
 import { CourseProps } from '@/types/api'
 import MiniCourseCard from './MiniCourseCard/MiniCourseCard'
 import SectionTitle from '@/components/SectionTitle/SectionTitle'
-const AlsoBought = ({ course }: { course: CourseProps }) => {
-	return (
+import Loader from '@/components/ui/loader'
+const AlsoBought = ({ courseId }: { courseId: string }) => {
+	const [otherCourses, setOtherCourses] = useState<null | CourseProps[]>(null)
+	const [loading, setLoading] = useState(true)
+	useEffect(() => {
+		const fetchCourse = async () => {
+			try {
+				const res = await fetch(`/api/also-bought?courseId=${courseId}`)
+				const data = await res.json()
+
+				const courses = data.courses
+				setOtherCourses(courses)
+				setLoading(false)
+			} catch (err) {
+				console.log(err)
+			}
+		}
+		if (courseId) {
+			fetchCourse()
+		}
+	}, [courseId])
+	return loading ? (
+		<div className='flex justify-center items-center w-full py-8'>
+			<Loader />
+		</div>
+	) : (
 		<div className='w-full'>
-			<SectionTitle>Course participants also bought</SectionTitle>
-			<div className='flex flex-col gap-4'>
-				<MiniCourseCard {...course} />
-				<MiniCourseCard {...course} />
-			</div>
+			{otherCourses && otherCourses.length > 0 && (
+				<>
+					{' '}
+					<SectionTitle>Course participants also bought</SectionTitle>
+					<div className='flex flex-col gap-4'>
+						{otherCourses?.map(item => {
+							return <MiniCourseCard key={item.id} {...item} />
+						})}
+					</div>
+				</>
+			)}
 		</div>
 	)
 }
