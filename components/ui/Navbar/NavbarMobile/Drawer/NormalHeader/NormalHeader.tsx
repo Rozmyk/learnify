7a790e6b858a9e02ce762aa7746082de92463eaa
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-16 18:10:22 +0100

    Wrap button by link, add href, add logic to close drawer

--- components/ui/Navbar/NavbarMobile/Drawer/NormalHeader/NormalHeader.tsx
+++ components/ui/Navbar/NavbarMobile/Drawer/NormalHeader/NormalHeader.tsx
@@ -1,14 +1,22 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-const NormalHeader = () => {
+const NormalHeader = ({ handleCloseDrawer }: { handleCloseDrawer: () => void }) => {
 	return (
 		<div className='flex flex-col gap-2 mb-4 '>
-			<Button variant='outline'>
-				<Link href={'/sign-in'}>Login</Link>
-			</Button>
-			<Button variant='outline'>
-				<Link href='/sign-up'>Register</Link>
-			</Button>
+			<span onClick={handleCloseDrawer}>
+				<Link className='w-full' href={'/sign-in'}>
+					<Button className='w-full' variant='outline'>
+						Login
+					</Button>
+				</Link>
+			</span>
+			<span onClick={handleCloseDrawer}>
+				<Link className='w-full' href='/sign-up'>
+					<Button className='w-full' variant='outline'>
+						Register
+					</Button>
+				</Link>
+			</span>
 		</div>
 	)
 }
