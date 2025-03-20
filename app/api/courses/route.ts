09004279ee06aea0e1df233ef5e18d1aa5fca6fd
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-20 17:17:21 +0100

    Add author_id to query

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -7,6 +7,7 @@ export async function GET(req: NextRequest) {
 	const { searchParams } = new URL(req.url)
 	const level = searchParams.get('instructional_level')
 	const language = searchParams.get('lang')
+	const author_id = searchParams.get('author_id')
 	const category = searchParams.get('category')
 	const sortBy = searchParams.get('sort')
 	const rating = searchParams.get('rating') ? Number(searchParams.get('rating')) : undefined
@@ -19,6 +20,9 @@ export async function GET(req: NextRequest) {
 
 	if (language) {
 		query = query.eq('language', language)
+	}
+	if (author_id) {
+		query = query.eq('author_id', author_id)
 	}
 	if (level) {
 		query = query.eq('level', level)
