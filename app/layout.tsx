ecd3d46273c616e6dd1eae1d933be864f493be65
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-21 12:58:47 +0100

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,35 @@ const geistSans = Geist({
 	display: 'swap',
 	subsets: ['latin'],
 })
+export const metadata = {
+	title: {
+		default: 'Learnify – Online Learning Platform',
+		template: '%s | Learnify',
+	},
+	description:
+		'Learnify helps you learn online with high-quality courses taught by experts. Boost your skills in development, design, marketing, and more.',
+	metadataBase: new URL('https://learnify-ochre.vercel.app/'),
+	openGraph: {
+		title: 'Learnify – Online Courses and Training',
+		description:
+			'Join Learnify to access a variety of online courses taught by professionals. Learn anytime, anywhere.',
+		url: 'https://learnify-ochre.vercel.app/',
+		siteName: 'Learnify',
+
+		locale: 'en_US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Learnify – Learn from experts',
+		description: 'Explore online courses on Learnify and learn from the best instructors in the industry.',
+		images: ['/og-image.jpg'],
+		creator: '@learnify',
+	},
+	icons: {
+		icon: '/favicon.ico',
+	},
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
