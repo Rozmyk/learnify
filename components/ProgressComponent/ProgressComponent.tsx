f035df02c2fbd176e074211606407731034e92c2
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-21 11:37:11 +0100

    Update background color

--- components/ProgressComponent/ProgressComponent.tsx
+++ components/ProgressComponent/ProgressComponent.tsx
@@ -8,7 +8,8 @@ interface ProgressComponentProps {
 
 const ProgressComponent = ({ value, className }: ProgressComponentProps) => {
 	return (
-		<Progress.Root className={clsx('relative overflow-hidden bg-primary rounded-full w-full h-0.5', className)}>
+		<Progress.Root
+			className={clsx('relative overflow-hidden bg-muted-foreground rounded-full w-full h-0.5', className)}>
 			<Progress.Indicator className='bg-purple-300 h-full transition-all duration-300' style={{ width: `${value}%` }} />
 		</Progress.Root>
 	)
