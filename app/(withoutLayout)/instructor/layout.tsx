f01be6d8cce827e8d8217fe0fe62361ce53294ba
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-22 18:11:22 +0100

    Create layout for instructor page

---
+++ app/(withoutLayout)/instructor/layout.tsx
@@ -0,0 +1,23 @@
+import InstructorDrawer from '@/components/InstructorCoursesPage/InstrcutorDrawer/InstructorDrawer'
+import InstructorNavbar from '@/components/InstructorCoursesPage/InstructorNavbar/InstructorNavbar'
+import PageContainer from '@/components/PageContainer/PageContainer'
+import Footer from '@/components/Footer/Footer'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+export default async function Layout({ children }: { children: React.ReactNode }) {
+	const supabase = await createClient()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
+	if (!user) {
+		redirect('/')
+	}
+	return (
+		<>
+			<InstructorNavbar userId={user.id} />
+			<InstructorDrawer />
+			<PageContainer>{children}</PageContainer>
+			<Footer />
+		</>
+	)
+}
