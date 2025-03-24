d46f211ac6e5a57cb68aa6d0d15028681aa3de94
Michał Rozmysłowski <kymzor@gmail.com>
2025-03-24 18:40:08 +0100

    Fix type error

--- app/(withoutLayout)/course/create/[step]/page.tsx
+++ app/(withoutLayout)/course/create/[step]/page.tsx
@@ -1,7 +1,7 @@
 import RenderStepsPage from '@/components/RenderStepsPage/RenderStepsPage'
 
-export default async function Page({ params }: { params: { step: string } }) {
-	const { step } = params
+export default async function Page({ params }: { params: Promise<{ step: string }> }) {
+	const step = (await params).step
 
 	return <RenderStepsPage step={step} />
 }
